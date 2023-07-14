21bda669c82398983b23a728f2487b1eecc2f1ba
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-07-14 18:19:43 +0200

    Station bounty console

---
+++ Content.Client/_NF/StationBounties/StationBountyBoundUserInterface.cs
@@ -0,0 +1,8 @@
+using Robust.Client.GameObjects;
+
+namespace Content.Client._NF.StationBounties;
+
+public sealed class StationBountyBoundUserInterface : BoundUserInterface
+{
+    public StationBountyBoundUserInterface(ClientUserInterfaceComponent owner, Enum uiKey) : base(owner, uiKey) { }
+}
