72d34363eb0f7456bb747eaacf3d742b71cf9da9
Alexander Evgrashin <aevgrashin@yandex.ru>
2023-07-14 23:47:40 +0200

    Bounties stuff

---
+++ Content.Client/_NF/StationBounties/BUI/StationBountyBoundUserInterface.cs
@@ -0,0 +1,28 @@
+using Content.Client._NF.StationBounties.UI;
+using Robust.Client.GameObjects;
+
+namespace Content.Client._NF.StationBounties.BUI;
+
+public sealed class StationBountyBoundUserInterface : BoundUserInterface
+{
+    public StationBountyBoundUserInterface(ClientUserInterfaceComponent owner, Enum uiKey) : base(owner, uiKey) { }
+    private StationBountyMenu? _menu;
+
+    protected override void Open()
+    {
+        base.Open();
+
+        _menu = new StationBountyMenu();
+        _menu.OnClose += Close;
+        _menu.OpenCentered();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        if (disposing)
+        {
+            _menu?.Dispose();
+        }
+    }
+}
--- Content.Client/_NF/StationBounties/StationBountyBoundUserInterface.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Client.GameObjects;
-
-namespace Content.Client._NF.StationBounties;
-
-public sealed class StationBountyBoundUserInterface : BoundUserInterface
-{
-    public StationBountyBoundUserInterface(ClientUserInterfaceComponent owner, Enum uiKey) : base(owner, uiKey) { }
-}
