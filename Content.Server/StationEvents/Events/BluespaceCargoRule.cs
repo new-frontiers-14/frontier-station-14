044b826e144bf7d888ff0b68d59b25b8bca349bd
Dvir <dvirf01@gmail.com>
2023-09-09 01:50:21 +0300

    Event

--- Content.Server/StationEvents/Events/BluespaceCargoRule.cs
+++ Content.Server/StationEvents/Events/BluespaceCargoRule.cs
@@ -1,23 +1,22 @@
 using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Station.Components;
 using Content.Server.StationEvents.Components;
+using Content.Shared.Physics;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Physics.Components;
+using Robust.Shared.Physics;
 using Robust.Shared.Random;
-//using Content.Shared.Physics;
-//using Robust.Shared.Map.Components;
-//using Robust.Shared.Physics.Components;
-//using Robust.Shared.Physics;
-//using Robust.Shared.Configuration;
-//using Content.Server.Station.Components;
-//using Content.Server.Atmos.EntitySystems;
-//using Robust.Shared.Map;
-//using Content.Shared.Maps;
+using Robust.Shared.Configuration;
+using Content.Server.Atmos.EntitySystems;
+using Content.Shared.CCVar;
 
 namespace Content.Server.StationEvents.Events;
 
 public sealed class BluespaceCargoRule : StationEventSystem<BluespaceCargoRuleComponent>
 {
-    //[Dependency] private readonly IConfigurationManager _configuration = default!;
-    //[Dependency] private readonly AtmosphereSystem _atmosphere = default!;
-    //[Dependency] protected readonly IRobustRandom Random = default!;
+    [Dependency] private readonly IConfigurationManager _configuration = default!;
+    [Dependency] private readonly AtmosphereSystem _atmosphere = default!;
+    [Dependency] protected readonly IRobustRandom Random = default!;
     //[Dependency] private readonly ITileDefinitionManager _tileDefinitionManager = default!;
     //[Dependency] private readonly IMapManager _mapManager = default!;
 
@@ -33,16 +32,74 @@ public sealed class BluespaceCargoRule : StationEventSystem<BluespaceCargoRuleCo
     {
         base.Started(uid, component, gameRule, args);
 
+        if (!TryGetRandomStation(out var chosenStation, HasComp<StationJobsComponent>))
+            return;
+
+        if (!TryComp<StationDataComponent>(chosenStation, out var stationData))
+            return;
+
+        var grid = StationSystem.GetLargestGrid(stationData);
+
+        if (grid is null)
+            return;
+
         var amountToSpawn = Math.Max(1, (int) MathF.Round(GetSeverityModifier() / 1.5f));
         for (var i = 0; i < amountToSpawn; i++)
         {
-            if (!TryFindRandomTile(out _, out _, out _, out var coords))
-                return;
-
-            //Spawn(component.CargoSpawnerPrototype, coords);
-            //Spawn(component.CargoFlashPrototype, coords);
-
-            //Sawmill.Info($"Spawning random cargo at {coords}");
+            SpawnOnRandomGridLocation(grid.Value, component.CargoSpawnerPrototype, component.CargoFlashPrototype);
         }
     }
+
+    public void SpawnOnRandomGridLocation(EntityUid grid, string toSpawn, string toSpawnFlash)
+    {
+        if (!TryComp<MapGridComponent>(grid, out var gridComp))
+            return;
+
+        var xform = Transform(grid);
+
+        var targetCoords = xform.Coordinates;
+        var gridBounds = gridComp.LocalAABB.Scale(_configuration.GetCVar(CCVars.AnomalyGenerationGridBoundsScale));
+
+        for (var i = 0; i < 25; i++)
+        {
+            var randomX = Random.Next((int) gridBounds.Left, (int) gridBounds.Right);
+            var randomY = Random.Next((int) gridBounds.Bottom, (int) gridBounds.Top);
+
+            var tile = new Vector2i(randomX, randomY);
+
+            // no air-blocked areas.
+            if (_atmosphere.IsTileSpace(grid, xform.MapUid, tile, mapGridComp: gridComp) ||
+                _atmosphere.IsTileAirBlocked(grid, tile, mapGridComp: gridComp))
+            {
+                continue;
+            }
+
+            // don't spawn inside of solid objects
+            var physQuery = GetEntityQuery<PhysicsComponent>();
+            var valid = true;
+            foreach (var ent in gridComp.GetAnchoredEntities(tile))
+            {
+                if (!physQuery.TryGetComponent(ent, out var body))
+                    continue;
+                if (body.BodyType != BodyType.Static ||
+                    !body.Hard ||
+                    (body.CollisionLayer & (int) CollisionGroup.Impassable) == 0)
+                    continue;
+
+                valid = false;
+                break;
+            }
+            if (!valid)
+                continue;
+
+            targetCoords = gridComp.GridTileToLocal(tile);
+            break;
+        }
+
+        Spawn(toSpawn, targetCoords);
+        Spawn(toSpawnFlash, targetCoords);
+
+        Sawmill.Info($"Spawning random cargo at {targetCoords}");
+    }
+
 }
