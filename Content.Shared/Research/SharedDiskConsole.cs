d16635367145640b4bc6b2e827561c18f4d86f93
Dvir <39403717+dvir001@users.noreply.github.com>
2023-10-29 19:42:06 +0200

    TechBalance (#507)
    * TechBalance
    
    * Update disk.yml

--- Content.Shared/Research/SharedDiskConsole.cs
+++ Content.Shared/Research/SharedDiskConsole.cs
@@ -1,4 +1,4 @@
-﻿using Robust.Shared.Serialization;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared.Research;
 
@@ -12,19 +12,29 @@ public enum DiskConsoleUiKey : byte
 public sealed class DiskConsoleBoundUserInterfaceState : BoundUserInterfaceState
 {
     public bool CanPrint;
+    public bool CanPrintRare;
     public int PointCost;
+    public int PointCostRare;
     public int ServerPoints;
 
-    public DiskConsoleBoundUserInterfaceState(int serverPoints, int pointCost, bool canPrint)
+    public DiskConsoleBoundUserInterfaceState(int serverPoints, int pointCost, int pointCostRare, bool canPrint, bool canPrintRare)
     {
         CanPrint = canPrint;
+        CanPrintRare = canPrintRare;
         PointCost = pointCost;
+        PointCostRare = pointCostRare;
         ServerPoints = serverPoints;
     }
 }
 
 [Serializable, NetSerializable]
 public sealed class DiskConsolePrintDiskMessage : BoundUserInterfaceMessage
+{
+
+}
+
+[Serializable, NetSerializable]
+public sealed class DiskConsolePrintRareDiskMessage : BoundUserInterfaceMessage
 {
 
 }
