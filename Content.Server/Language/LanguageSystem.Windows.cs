1570cc20a176219af5ec862320b6547d07aa7354
fox <daytimer253@gmail.com>
2023-12-09 23:00:21 +0300

    New UI controller for the language menu action

--- Content.Server/Language/LanguageSystem.Windows.cs
+++ Content.Server/Language/LanguageSystem.Windows.cs
@@ -10,36 +10,20 @@ public sealed partial class LanguageSystem : SharedLanguageSystem
 
     public void InitializeWindows()
     {
-        SubscribeLocalEvent<LanguageSpeakerComponent, LanguageMenuActionEvent>(MenuEvent);
+        SubscribeNetworkEvent<RequestLanguageMenuStateMessage>(OnLanguagesRequest);
     }
 
-    private void MenuEvent(EntityUid uid, LanguageSpeakerComponent component, LanguageMenuActionEvent args)
+    private void OnLanguagesRequest(RequestLanguageMenuStateMessage args, EntitySessionEventArgs session)
     {
-        if (!TryComp(uid, out ActorComponent? actor))
+        var uid = session.SenderSession.AttachedEntity;
+        if (uid == null)
             return;
 
-        EnsureComp<UserInterfaceComponent>(uid);
-
-        // TODO: does not work
-        _uiSystem.TryOpen(uid, LanguageMenuUiKey.Key, actor.PlayerSession);
-
-       UpdateUserInterface(uid, component, actor.PlayerSession);
-    }
-
-    private void UpdateUserInterface(EntityUid uid, LanguageSpeakerComponent component, ICommonSession? session = null)
-    {
-        var langs = GetLanguages(uid, component);
+        var langs = GetLanguages(uid.Value);
         if (langs == null)
             return;
 
-        if (session == null)
-        {
-            if (!TryComp<ActorComponent>(uid, out var actor))
-                return;
-            session = actor.PlayerSession;
-        }
-
-        var state = new LanguageMenuState(langs.CurrentLanguage, langs.SpokenLanguages);
-        _uiSystem.TrySetUiState(uid, LanguageMenuUiKey.Key, state, session);
+        var state = new LanguageMenuStateMessage(langs.CurrentLanguage, langs.SpokenLanguages);
+        RaiseNetworkEvent(state, uid.Value);
     }
 }
