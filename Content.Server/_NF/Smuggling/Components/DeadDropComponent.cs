3779a32bd54d327ca23d94ed908a9038ab9f8853
Checkraze <71046427+Cheackraze@users.noreply.github.com>
2023-12-15 15:39:18 -0500

    Smuggling Dead Drops (#703)
    * smuggling dead drops
    
    * sprites and fluff texts
    
    * license fix
    
    * fix the fix

---
+++ Content.Server/_NF/Smuggling/Components/DeadDropComponent.cs
@@ -0,0 +1,60 @@
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+
+namespace Content.Server._NF.Smuggling.Components;
+
+/// <summary>
+///     Store all bounty contracts information.
+/// </summary>
+[RegisterComponent]
+[Access(typeof(DeadDropSystem))]
+public sealed partial class DeadDropComponent : Component
+{
+    /// <summary>
+    ///     When the next drop will occur. Used internally.
+    /// </summary>
+    [DataField("nextDrop")]
+    public TimeSpan? NextDrop;
+
+    /// <summary>
+    ///     Minimum wait time in seconds to wait for the next dead drop.
+    /// </summary>
+    [DataField("minimumCoolDown")]
+    public int MinimumCoolDown = 600;
+
+    /// <summary>
+    ///     Max wait time in seconds to wait for the next dead drop.
+    /// </summary>
+    [DataField("maximumCoolDown")]
+    public int MaximumCoolDown = 2400;
+
+    /// <summary>
+    ///     Minimum distance to spawn the drop.
+    /// </summary>
+    [DataField("minimumDistance")]
+    public int MinimumDistance = 6500;
+
+    /// <summary>
+    ///     Max distance to spawn the drop.
+    /// </summary>
+    [DataField("maximumDistance")]
+    public int MaximumDistance = 9900;
+
+    /// <summary>
+    ///     The paper prototype to spawn.
+    /// </summary>
+    [DataField("hintPaper", customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
+    public string HintPaper = "PaperCargoInvoice";
+
+    /// <summary>
+    ///     Location of the grid to spawn in as the dead drop.
+    /// </summary>
+    [DataField("dropGrid")]
+    public string DropGrid = "/Maps/deaddrop.yml";
+
+    /// <summary>
+    ///     The color of your grid. the name should be set by the mapper when mapping.
+    /// </summary>
+    [DataField("color")]
+    public Color Color = new Color(225, 15, 155);
+}
