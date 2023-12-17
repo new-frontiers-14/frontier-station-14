70476d2705b91b4cfb3edc192e21ab178f34790c
checkraze <jgrigg87@gmail.com>
2023-12-17 17:04:40 -0500

    comments

--- Content.Server/_NF/Smuggling/DeadDropSystem.cs
+++ Content.Server/_NF/Smuggling/DeadDropSystem.cs
@@ -44,6 +44,7 @@ public sealed class DeadDropSystem : EntitySystem
 
     private void OnStartup(EntityUid paintingUid, DeadDropComponent component, ComponentStartup _)
     {
+        //set up the timing of the first activation
         component.NextDrop = _timing.CurTime + TimeSpan.FromSeconds(_random.Next(component.MinimumCoolDown, component.MaximumCoolDown));
     }
 
@@ -52,6 +53,7 @@ public sealed class DeadDropSystem : EntitySystem
         if (!args.CanInteract || !args.CanAccess || args.Hands == null || _timing.CurTime < component.NextDrop)
             return;
 
+        //here we build our dynamic verb. Using the object's sprite for now to make it more dynamic for the moment.
         InteractionVerb searchVerb = new()
         {
             IconEntity = GetNetEntity(uid),
@@ -63,11 +65,14 @@ public sealed class DeadDropSystem : EntitySystem
         args.Verbs.Add(searchVerb);
     }
 
+    //spawning the dead drop.
     private void SendDeadDrop(EntityUid uid, DeadDropComponent component, EntityUid user, HandsComponent hands)
     {
+        //simple check to make sure we dont allow multiple activations from a desynced verb window.
         if (_timing.CurTime < component.NextDrop)
             return;
 
+        //relying entirely on shipyard capabilities, including using the shipyard map to spawn the items and ftl to bring em in
         if (_shipyard.ShipyardMap is not MapId shipyardMap)
             return;
 
@@ -76,12 +81,16 @@ public sealed class DeadDropSystem : EntitySystem
             LoadMap = false,
         };
 
+        //load whatever grid was specified on the component, either a special dead drop or default
         if (!_map.TryLoad(shipyardMap, component.DropGrid, out var gridUids, options))
             return;
 
+        //setup the radar properties
         _shuttle.SetIFFColor(gridUids[0], component.Color);
         _shuttle.AddIFFFlag(gridUids[0], IFFFlags.HideLabel);
 
+        //this is where we set up all the information that FTL is going to need, including a new null entitiy as a destination target because FTL needs it for reasons?
+        //dont ask me im just fulfilling FTL requirements.
         var dropLocation = _random.NextVector2(component.MinimumDistance, component.MaximumDistance);
         var mapId = Transform(user).MapID;
         var coords = new MapCoordinates(dropLocation, mapId);
@@ -92,14 +101,15 @@ public sealed class DeadDropSystem : EntitySystem
             _shuttle.FTLTravel(gridUids[0], shuttle, location, 5.5f, 35f);
         }
 
+        //tattle on the smuggler here, but obfuscate it a bit if possible to just the grid it was summoned from.
         var channel = _prototypeManager.Index<RadioChannelPrototype>("Security");
         var sender = Transform(user).GridUid ?? uid;
 
         _radio.SendRadioMessage(sender, Loc.GetString("deaddrop-security-report"), channel, uid);
         _adminLogger.Add(LogType.Action, LogImpact.Medium, $"{ToPrettyString(user)} activated a dead drop from {ToPrettyString(uid)} at {Transform(uid).Coordinates.ToString()}");
 
+        // here we are just building a string for the hint paper so that it looks pretty and RP-like on the paper itself.
         var dropHint = new StringBuilder();
-
         dropHint.AppendLine(Loc.GetString("deaddrop-hint-pretext"));
         dropHint.AppendLine();
         dropHint.AppendLine(dropLocation.ToString());
@@ -113,6 +123,7 @@ public sealed class DeadDropSystem : EntitySystem
         _meta.SetEntityDescription(paper, Loc.GetString("deaddrop-hint-desc"));
         _hands.PickupOrDrop(user, paper, handsComp: hands);
 
+        //reset the timer
         component.NextDrop = _timing.CurTime + TimeSpan.FromSeconds(_random.Next(component.MinimumCoolDown, component.MaximumCoolDown));
     }
 }
