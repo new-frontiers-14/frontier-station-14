c33a4a867120ce8c14667b90f960b9c3ff55a8f7
Dvir <dvirf01@gmail.com>
2023-12-17 21:46:49 +0200

    Crispy Cat

---
+++ Content.Server/Traits/Assorted/FriedTraitSystem.cs
@@ -0,0 +1,22 @@
+using Content.Server.Kitchen.EntitySystems;
+
+namespace Content.Server.Traits.Assorted;
+
+/// <summary>
+/// This handles fried trait, causing the affected to look crispy.
+/// </summary>
+public sealed class FriedTraitSystem : EntitySystem
+{
+    [Dependency] private readonly DeepFryerSystem _deepFryerSystem = default!;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<FriedTraitComponent, ComponentStartup>(SetupFriedTrait);
+    }
+
+    private void SetupFriedTrait(EntityUid uid, FriedTraitComponent component, ComponentStartup args)
+    {
+        _deepFryerSystem.MakeCrispy(uid);
+    }
+}
