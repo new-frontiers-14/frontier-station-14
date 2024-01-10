277d967e09402e3dd1e9ba630a9d4ca635f1ce19
Crepe <mrcreaper101@gmail.com>
2024-01-10 02:12:28 +0200

    updated icon.png-s as well

---
+++ Resources/Textures/Logo/icon/resize.sh
@@ -0,0 +1,8 @@
+#pacman -Syu --noconfirm imagemagick
+convert -resize 16x16 icon.png icon-16x16.png
+convert -resize 24x24 icon.png icon-24x24.png
+convert -resize 32x32 icon.png icon-32x32.png
+convert -resize 48x48 icon.png icon-48x48.png
+convert -resize 64x64 icon.png icon-64x64.png
+convert -resize 128x128 icon.png icon-128x128.png
+convert -resize 256x256 icon.png icon-256x256.png
