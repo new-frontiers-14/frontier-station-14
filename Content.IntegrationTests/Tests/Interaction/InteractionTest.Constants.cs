b55fa9eb81aed982f2f4dae451637e3481d22628
checkraze <jgrigg87@gmail.com>
2024-02-02 16:16:01 -0500

    Revert "kill machine parts + migrate machine part spawners to salvage loot spawner (#23752)"
    This reverts commit df1d8c36

--- Content.IntegrationTests/Tests/Interaction/InteractionTest.Constants.cs
+++ Content.IntegrationTests/Tests/Interaction/InteractionTest.Constants.cs
@@ -1,3 +1,4 @@
+
 namespace Content.IntegrationTests.Tests.Interaction;
 
 // This partial class contains various constant prototype IDs common to interaction tests.
@@ -27,8 +28,12 @@ public abstract partial class InteractionTest
 
     // Parts
     protected const string Bin1 = "MatterBinStockPart";
+    protected const string Bin4 = "BluespaceMatterBinStockPart";
     protected const string Cap1 = "CapacitorStockPart";
+    protected const string Cap4 = "QuadraticCapacitorStockPart";
     protected const string Manipulator1 = "MicroManipulatorStockPart";
+    protected const string Manipulator4 = "FemtoManipulatorStockPart";
     protected const string Battery1 = "PowerCellSmall";
     protected const string Battery4 = "PowerCellHyper";
 }
+
