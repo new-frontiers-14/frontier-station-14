a689f3f2c7f70cdce3a2e110f132e2dd52d6bab8
Dvir <dvirf01@gmail.com>
2024-02-08 19:23:29 +0200

    Update config.py

--- .github/mapchecker/config.py
+++ .github/mapchecker/config.py
@@ -2,12 +2,24 @@
 ILLEGAL_MATCHES = [
     "DO NOT MAP",
     "DEBUG",
+	"CaptainSabre",
+	"ClothingBeltSheathFilled",
+	"WeaponAntiqueLaser",
+	"Kammerer",
+	"MagazinePistolHighCapacity",
+	"MagazinePistolHighCapacityRubber",
+	"EncryptionKeyCommand",
+	"SurveillanceCameraWireless",
+	"CrewMonitoringServer",
+	"PDA",
 ]
 # List of matchers that are illegal to use, unless the map is a ship and the ship belongs to the keyed shipyard.
 CONDITIONALLY_ILLEGAL_MATCHES = {
     "Security": [  # These matchers are illegal unless the ship is part of the security shipyard.
         "Security",  # Anything with the word security in it should also only be appearing on security ships.
         "Plastitanium",  # Plastitanium walls should only be appearing on security ships.
+		"HoloprojectorSecurity",
+		"EncryptionKeySecurity",
     ],
     "BlackMarket": [
         "Plastitanium",  # And also on blackmarket ships cause syndicate.
