ba2c14b60b978527f2cfeca9a2d07afc39d68fb3
fox <daytimer253@gmail.com>
2024-03-06 22:23:38 +0300

    Remove the contest from shoving; remove unused contests

--- Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
+++ Content.Server/Weapons/Melee/MeleeWeaponSystem.cs
@@ -223,10 +223,7 @@ public sealed class MeleeWeaponSystem : SharedMeleeWeaponSystem
         if (HasComp<DisarmProneComponent>(disarmed))
             return 0.0f;
 
-        // var chance = disarmerComp.BaseDisarmFailChance;
-        // Frontier - restored from pre-#25425
-        var contestResults = 1 - _contests.OverallStrengthContest(disarmer, disarmed);
-        float chance = (disarmerComp.BaseDisarmFailChance + contestResults);
+        var chance = disarmerComp.BaseDisarmFailChance;
 
         if (inTargetHand != null && TryComp<DisarmMalusComponent>(inTargetHand, out var malus))
         {
