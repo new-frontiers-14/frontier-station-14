a44929247799e7cab0095d947ad2426b96160154
deltanedas <39013340+deltanedas@users.noreply.github.com>
2024-04-10 20:06:31 +0000

    fix lots of door access (#26858)
    * dirty after calling SetAccesses
    
    * fix door access
    
    * D
    
    * pro ops
    
    * nukeop
    
    ---------
    
    Co-authored-by: deltanedas <@deltanedas:kde.org>

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -79,6 +79,7 @@ public sealed partial class IdCardConsoleComponent : Component
         "Salvage",
         "Security",
         "Service",
+        "StationTrafficController", // Frontier
         "Theatre",
     };
 
