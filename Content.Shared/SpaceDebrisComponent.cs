4b83905851b35fb75ef5b5ee9315baac0d05994d
Zekins <136648667+Zekins3366@users.noreply.github.com>
2024-04-18 17:42:29 +0300

    Create SpaceDebrisComponent.cs

---
+++ Content.Shared/SpaceDebrisComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Server.Worldgen.Components.Debris;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SpaceDebrisComponent : Component
+{
+}
