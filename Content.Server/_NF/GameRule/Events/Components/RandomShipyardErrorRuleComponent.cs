8ab4a452eca1f692847b0b6f155d1ddc278f5424
noverd <dasfasru@gmail.com>
2024-04-19 08:15:51 +0300

    Рандомное событие

---
+++ Content.Server/_NF/GameRule/Events/Components/RandomShipyardErrorRuleComponent.cs
@@ -0,0 +1,22 @@
+namespace Content.Server._NF.GameRule.Events.Components;
+
+[RegisterComponent, Access(typeof(RandomShipyardErrorRule))]
+public sealed partial class RandomShipyardErrorRuleComponent : Component
+{
+    /// <summary>
+    /// Min vessels count for spawning
+    /// </summary>
+    [DataField("minGridCount")]
+    public int MinGridsCount = 2;
+    /// <summary>
+    /// Man vessels count for spawning
+    /// </summary>
+    [DataField("maxGridCount")]
+    public int MaxGridsCount = 5;
+
+    /// <summary>
+    /// The grid in question, set after starting the event
+    /// </summary>
+    [DataField("gridUid")]
+    public List<EntityUid?> GridUids = new();
+}
