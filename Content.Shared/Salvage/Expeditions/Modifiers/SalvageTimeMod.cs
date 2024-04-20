e353789f98238bde3f23b1ad57fc54bfbf3204a6
steinhauer.erhard <steinhauer.erhard@gmail.com>
2024-04-21 01:54:36 +0300

    adjustments

--- Content.Shared/Salvage/Expeditions/Modifiers/SalvageTimeMod.cs
+++ Content.Shared/Salvage/Expeditions/Modifiers/SalvageTimeMod.cs
@@ -11,14 +11,14 @@ public sealed class SalvageTimeMod : IPrototype, ISalvageMod
 
     /// <summary>
     /// Cost for difficulty modifiers.
-    /// Frontier: expedition timer min set to 930 and max to 1080
+    /// Frontier: expedition timer min set to 900 and max to 930
     /// </summary>
     [DataField("cost")]
     public float Cost { get; private set; }
 
     [DataField("minDuration")]
-    public int MinDuration = 930;
+    public int MinDuration = 900;
 
     [DataField("maxDuration")]
-    public int MaxDuration = 1080;
+    public int MaxDuration = 930;
 }
