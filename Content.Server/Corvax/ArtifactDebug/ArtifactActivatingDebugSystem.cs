cfd8d2d0c603e736c7ff1c0ea62888a1e3f84e1a
FN <firenamefn@gmail.com>
2024-04-30 23:30:33 +0700

    Added debug command

---
+++ Content.Server/Corvax/ArtifactDebug/ArtifactActivatingDebugSystem.cs
@@ -0,0 +1,18 @@
+﻿using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Events;
+using Content.Shared.Interaction.Events;
+
+namespace Content.Server.Corvax.Debug;
+
+public sealed class ArtifactActivatingDebugSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        SubscribeLocalEvent<ThrowArtifactComponent, UseInHandEvent>(OnUseInHand);
+    }
+
+    private void OnUseInHand(EntityUid entity, ThrowArtifactComponent component, UseInHandEvent e)
+    {
+        RaiseLocalEvent<ArtifactActivatedEvent>(entity, new());
+    }
+}
---
+++ Content.Server/Corvax/ArtifactDebug/SpawnThrowArtifactItemCommand.cs
@@ -0,0 +1,31 @@
+﻿using Content.Server.Administration;
+using Content.Server.Xenoarchaeology.XenoArtifacts.Effects.Components;
+using Content.Shared.Administration;
+using Content.Shared.Mind;
+using Content.Shared.Players;
+using Robust.Shared.Console;
+using Robust.Shared.Map;
+
+namespace Content.Server.Corvax.Debug;
+
+[AdminCommand(AdminFlags.Debug)]
+public sealed class SpawnThrowArtifactItemCommand : IConsoleCommand
+{
+    [Dependency] private readonly EntityManager _manager = default!;
+
+    public string Command => "spawnthrowartifactitem";
+
+    public string Description => "Spawns item that can emit ThrowArtifact.";
+
+    public string Help => $"Usage: {Command}";
+
+    public void Execute(IConsoleShell console, string arg, string[] args)
+    {
+        if (console.Player is null || !_manager.TryGetComponent<MindComponent>(console.Player.GetMind(), out var mind) || mind.CurrentEntity is null)
+            return;
+
+        var entity = _manager.SpawnEntity("ToyNuke", new EntityCoordinates(mind.CurrentEntity.Value, new()));
+
+        _manager.AddComponent<ThrowArtifactComponent>(entity);
+    }
+}
