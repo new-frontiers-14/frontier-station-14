65cb177ce4c738ae48ea0e50c97db0c956d50d02
Dmitriy <108795395+Sh1ntra@users.noreply.github.com>
2024-05-04 01:55:37 +0300

    Update AutoDeleteSystem.cs

--- Content.Server/Corvax/AutoDeleteItems/AutoDeleteSystem.cs
+++ Content.Server/Corvax/AutoDeleteItems/AutoDeleteSystem.cs
@@ -2,6 +2,8 @@ using Content.Server.Construction.Completions;
 using Content.Shared.Humanoid;
 using Content.Shared.Mobs.Systems;
 using Robust.Shared.Timing;
+using Robust.Shared.Player;
+using Content.Shared.SSDIndicator;
 
 namespace Content.Server.Corvax.AutoDeleteItems;
 
@@ -28,40 +30,60 @@ public sealed class AutoDeleteSystem : EntitySystem
             {
                 return;
             }
-            var humanoids = new HashSet<Entity<HumanoidAppearanceComponent>>();
 
             if (autoDeleteComponent.NextTimeToCheck > _gameTiming.CurTime)
                 return;
-            _lookup.GetEntitiesInRange(xform.Coordinates, autoDeleteComponent.DistanceToCheck, humanoids);
 
-            if (humanoids.Count > 0)
-                autoDeleteComponent.IsHumanoidNear = true;
-            else
-                autoDeleteComponent.IsHumanoidNear = false;
-
-            if (autoDeleteComponent.IsHumanoidNear == false && autoDeleteComponent.ReadyToDelete == true && autoDeleteComponent.NextTimeToDelete < _gameTiming.CurTime)
+            SSDIndicatorComponent? ssdComponent = null;
+            //if (!TryComp<SSDIndicatorComponent>(xform., out var ssd))
+            //    continue;
+            foreach (var iterator in _lookup.GetEntitiesInRange<HumanoidAppearanceComponent>(xform.Coordinates, autoDeleteComponent.DistanceToCheck))
             {
-                EntityManager.DeleteEntity(uid);
+                if (TryComp(iterator, out ssdComponent) && ssdComponent.IsSSD == true)
+                {
+                    autoDeleteComponent.IsSSDNear = true;
+                }
+                else if(TryComp(iterator, out ssdComponent) && ssdComponent.IsSSD == false)
+                {
+                    autoDeleteComponent.IsSSDNear = false;
+                }
+                    
+                if (iterator.Owner == uid)
+                    continue;
+
+                var humanoids = new HashSet<Entity<HumanoidAppearanceComponent>>();
+
+                _lookup.GetEntitiesInRange(xform.Coordinates, autoDeleteComponent.DistanceToCheck, humanoids);
+
+                if (humanoids.Count > 0 && !autoDeleteComponent.IsSSDNear)
+                    autoDeleteComponent.IsHumanoidNear = true;
+                else
+                    autoDeleteComponent.IsHumanoidNear = false;
+
+                if (autoDeleteComponent.IsHumanoidNear == false && autoDeleteComponent.ReadyToDelete == true && autoDeleteComponent.NextTimeToDelete < _gameTiming.CurTime)
+                {
+                    EntityManager.DeleteEntity(uid);
+                }
+                if (autoDeleteComponent.IsSSDNear == true)
+                {
+                    autoDeleteComponent.IsHumanoidNear = false;
+                }
+                if (autoDeleteComponent.IsHumanoidNear == true && autoDeleteComponent.IsSSDNear == false)
+                {
+                    if (autoDeleteComponent.ReadyToDelete == true)
+                        autoDeleteComponent.ReadyToDelete = false;
+
+                    autoDeleteComponent.NextTimeToDelete = _gameTiming.CurTime + autoDeleteComponent.DelayToDelete;
+                    autoDeleteComponent.NextTimeToCheck = _gameTiming.CurTime + autoDeleteComponent.DelayToCheck;
+                }
+
+                if (autoDeleteComponent.IsHumanoidNear == false && autoDeleteComponent.ReadyToDelete == false)
+                {
+                    autoDeleteComponent.NextTimeToDelete = _gameTiming.CurTime + autoDeleteComponent.DelayToDelete;
+                    autoDeleteComponent.NextTimeToCheck = _gameTiming.CurTime + autoDeleteComponent.DelayToCheck;
+                    autoDeleteComponent.ReadyToDelete = true;
+                }
             }
-
-            if (autoDeleteComponent.IsHumanoidNear == true)
-            {
-                if (autoDeleteComponent.ReadyToDelete == true)
-                    autoDeleteComponent.ReadyToDelete = false;
-
-                autoDeleteComponent.NextTimeToDelete = _gameTiming.CurTime + autoDeleteComponent.DelayToDelete;
-                autoDeleteComponent.NextTimeToCheck = _gameTiming.CurTime + autoDeleteComponent.DelayToCheck;
-            }
-
-            if (autoDeleteComponent.IsHumanoidNear == false && autoDeleteComponent.ReadyToDelete == false)
-            {
-                autoDeleteComponent.NextTimeToDelete = _gameTiming.CurTime + autoDeleteComponent.DelayToDelete;
-                autoDeleteComponent.NextTimeToCheck = _gameTiming.CurTime + autoDeleteComponent.DelayToCheck;
-                autoDeleteComponent.ReadyToDelete = true;
-            }
-
-            //autoDeleteComponent.NextTimeToDelete = _gameTiming.CurTime + autoDeleteComponent.DelayToDelete;
-            //autoDeleteComponent.NextTimeToCheck = _gameTiming.CurTime + autoDeleteComponent.DelayToCheck;
         }
     }
 }
