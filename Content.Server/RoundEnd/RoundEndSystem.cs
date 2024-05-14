29a1eb61f908e958842f96f220c47f61ab31364b
Kill_Me_I_Noobs <118206719+Vonsant@users.noreply.github.com>
2024-05-14 10:10:36 +0300

    Увеличение времени прилёта автоэвака с 10 до 30

--- Content.Server/RoundEnd/RoundEndSystem.cs
+++ Content.Server/RoundEnd/RoundEndSystem.cs
@@ -48,7 +48,7 @@ namespace Content.Server.RoundEnd
         /// <summary>
         /// Countdown to use where there is no station alert countdown to be found.
         /// </summary>
-        public TimeSpan DefaultCountdownDuration { get; set; } = TimeSpan.FromMinutes(10);
+        public TimeSpan DefaultCountdownDuration { get; set; } = TimeSpan.FromMinutes(30);
 
         private CancellationTokenSource? _countdownTokenSource = null;
         private CancellationTokenSource? _cooldownTokenSource = null;
