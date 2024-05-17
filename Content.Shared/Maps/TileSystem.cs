9e7b29090516612f4bf4559accf1c7c86b2b4197
Dvir <dvirf01@gmail.com>
2024-05-17 13:40:03 +0300

    Update TileSystem.cs

--- Content.Shared/Maps/TileSystem.cs
+++ Content.Shared/Maps/TileSystem.cs
@@ -6,6 +6,7 @@ using Robust.Shared.Map;
 using Robust.Shared.Map.Components;
 using Robust.Shared.Random;
 using Robust.Shared.Utility;
+using Content.Shared.Tiles; // Frontier
 
 namespace Content.Shared.Maps;
 
