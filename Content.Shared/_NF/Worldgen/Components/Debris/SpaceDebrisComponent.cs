4220e4dbc45ce44fc011204395d8cc8f550d115c
Dvir <39403717+dvir001@users.noreply.github.com>
2024-05-18 05:12:41 +0300

    Optimize Debris Code (#1374)
    * Optimize With Corvax Code
    
    * Update base_debris.yml
    
    * Update base_debris.yml

---
+++ Content.Shared/_NF/Worldgen/Components/Debris/SpaceDebrisComponent.cs
@@ -0,0 +1,8 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Server._NF.Worldgen.Components.Debris;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SpaceDebrisComponent : Component
+{
+}
