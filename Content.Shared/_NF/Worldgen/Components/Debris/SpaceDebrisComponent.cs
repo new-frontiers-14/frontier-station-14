b89bf7e6b281a5e613f15359b2a81717705eb3a7
Checkraze <71046427+Cheackraze@users.noreply.github.com>
2024-05-18 12:31:57 -0400

    Revert "Optimize Debris Code (#1374)" (#1383)
    This reverts commit 4220e4dbc45ce44fc011204395d8cc8f550d115c.

--- Content.Shared/_NF/Worldgen/Components/Debris/SpaceDebrisComponent.cs
+++
@@ -1,8 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Server._NF.Worldgen.Components.Debris;
-
-[RegisterComponent, NetworkedComponent]
-public sealed partial class SpaceDebrisComponent : Component
-{
-}
