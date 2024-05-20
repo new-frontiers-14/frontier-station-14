db06089343cf5b31934965f105e42942555a125e
Dvir <39403717+dvir001@users.noreply.github.com>
2024-05-20 23:14:32 +0300

    Update Content.Client/Shuttles/UI/BaseShuttleControl.xaml.cs
    Co-authored-by: whatston3 <166147148+whatston3@users.noreply.github.com>

--- Content.Client/Shuttles/UI/BaseShuttleControl.xaml.cs
+++ Content.Client/Shuttles/UI/BaseShuttleControl.xaml.cs
@@ -120,7 +120,7 @@ public partial class BaseShuttleControl : MapGridControl
     {
         var origin = ScalePosition(-new Vector2(Offset.X, -Offset.Y));
         var aExtent = (angle - Math.Tau / 4).ToVec() * ScaledMinimapRadius * 1.42f;
-        var lineColor = Color.Red.WithAlpha(0.02f);
+        var lineColor = Color.Red.WithAlpha(0.05f);
         handle.DrawLine(origin, origin + aExtent, lineColor);
     }
 
