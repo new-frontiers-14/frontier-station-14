dd14cae1cefe33d5faf402d533440ad9c91ad9ae
blue3212 <luckywill339@gmail.com>
2024-05-27 17:29:12 -0400

    Add reset reagent button for the reagent filter UI

---
+++ Content.Shared/_NF/Chemistry/Events/SharedReagentWhitelistResetMessage.cs
@@ -0,0 +1,18 @@
+using Content.Shared.Chemistry.Reagent;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._NF.Chemistry.Events;
+
+
+/// <summary>
+///     Sends a message to change the associated injector component's ReagentWhitelist to the newReagent
+/// </summary>
+[Serializable, NetSerializable]
+public sealed class ReagentWhitelistResetMessage : BoundUserInterfaceMessage
+{
+    public ReagentWhitelistResetMessage()
+    {
+
+    }
+}
--- Resources/Locale/en-US/_NF/chemicals/ui/change-reagent-whitelist.ftl
+++ Resources/Locale/en-US/_NF/chemicals/ui/change-reagent-whitelist.ftl
@@ -4,10 +4,14 @@
 
 ui-change-reagent-whitelist-title = Reagent Filter
 
-## Text for the button when there is a selected reagent
+## Text for the apply button when there is a selected reagent
 
 ui-change-reagent-whitelist-apply = Only Draw {$reagent}
 
-##
+## Text for the apply button when there is no selected reagent
 
 ui-change-reagent-whitelist-apply-missing-reagent = Select a Reagent
+
+## Text for the reset button
+
+ui-change-reagent-whitelist-resetbutton-text = Reset Reagent
