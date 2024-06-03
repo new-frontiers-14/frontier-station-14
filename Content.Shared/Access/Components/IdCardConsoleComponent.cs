945cc87c287276de94fb72556edbe26d4b555df7
neuPanda <spainman0@yahoo.com>
2024-06-03 16:41:44 -0400

    removed: Atmospherics and Engineering from ID console

--- Content.Shared/Access/Components/IdCardConsoleComponent.cs
+++ Content.Shared/Access/Components/IdCardConsoleComponent.cs
@@ -45,7 +45,7 @@ public sealed partial class IdCardConsoleComponent : Component
     public List<ProtoId<AccessLevelPrototype>> AccessLevels = new()
     {
         "Armory",
-        "Atmospherics",
+        //"Atmospherics",
         "Bailiff", // Frontier
         //"Bar",
         "Brig",
@@ -58,7 +58,7 @@ public sealed partial class IdCardConsoleComponent : Component
         //"ChiefMedicalOfficer",
         "Command",
         //"Cryogenics",
-        "Engineering",
+        //"Engineering",
         "External",
         "Frontier", // Frontier
         "HeadOfPersonnel",
