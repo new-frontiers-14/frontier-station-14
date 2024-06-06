73f4881669b630df8f9640f45de04866ff761cfe
blue3212 <luckywill339@gmail.com>
2024-06-05 22:20:30 -0400

    Fix comment

--- Content.Shared/_NF/Chemistry/Events/SharedReagentWhitelistResetMessage.cs
+++ Content.Shared/_NF/Chemistry/Events/SharedReagentWhitelistResetMessage.cs
@@ -6,7 +6,8 @@ namespace Content.Shared._NF.Chemistry.Events;
 
 
 /// <summary>
-///     Sends a message to change the associated injector component's ReagentWhitelist to the newReagent
+///     Sends a message to change the associated injector component's ReagentWhitelist to null,
+///     allowing all Reagents to be drawn by that injector
 /// </summary>
 [Serializable, NetSerializable]
 public sealed class ReagentWhitelistResetMessage : BoundUserInterfaceMessage
