0cff49042df0858abb1eff838cf1766a227093b9
Whatstone <whatston3@gmail.com>
2024-06-07 10:32:59 -0400

    faith injector, holywater reaction needs BibleUser

---
+++ Content.Server/_NF/Implants/BibleUserImplantSystem.cs
@@ -0,0 +1,37 @@
+using Content.Shared.Implants;
+using Content.Shared.Implants.Components;
+using Content.Server.Bible.Components;
+using Robust.Shared.Containers;
+
+namespace Content.Server.Implants;
+
+public sealed class BibleUserImplantSystem : EntitySystem
+{
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<BibleUserImplantComponent, ImplantImplantedEvent>(OnInsert);
+        // We need to remove the BibleUserComponent from the owner before the implant
+        // is removed, so we need to execute before the SubdermalImplantSystem.
+        SubscribeLocalEvent<BibleUserImplantComponent, EntGotRemovedFromContainerMessage>(OnRemove, before: new[] { typeof(SubdermalImplantSystem) });
+    }
+
+    private void OnInsert(EntityUid uid, BibleUserImplantComponent component, ImplantImplantedEvent args)
+    {
+        if (!args.Implanted.HasValue)
+            return;
+
+        var bibleUserComp = EnsureComp<BibleUserComponent>(args.Implanted.Value);
+        Dirty(args.Implanted.Value, bibleUserComp);
+    }
+
+    private void OnRemove(EntityUid uid, BibleUserImplantComponent component, EntGotRemovedFromContainerMessage args)
+    {
+        if (!TryComp<SubdermalImplantComponent>(uid, out var implanted) || implanted.ImplantedEntity == null)
+            return;
+
+        RemComp<BibleUserComponent>(implanted.ImplantedEntity.Value);
+    }
+}
---
+++ Content.Shared/_NF/Implants/BibleUserImplantComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Implants.Components;
+
+/// <summary>
+/// Implant for a person to get BibleUser status
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class BibleUserImplantComponent : Component
+{
+}
