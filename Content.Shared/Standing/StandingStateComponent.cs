1ac73bc45933a610f436c246aff6e72fce75d641
user424242420 <142989209+user424242420@users.noreply.github.com>
2024-06-23 14:44:13 +0300

    Ляжания вторая попытка - ДОДЕЛАННОЕ, РАБОЧЕЕ ГАВНО (#325)
    * Не рабочее гавно в процессе
    
    * Не фикс говна
    
    * Update Content.Shared/Standing/StandingStateComponent.cs
    
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>
    
    * Update Content.Shared/LieDown/LyingDownComponent.cs
    
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>
    
    * Update Content.Shared/LieDown/SharedLieDownSystem.cs
    
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>
    
    * Revert "Update Content.Shared/LieDown/SharedLieDownSystem.cs"
    
    This reverts commit a133d9eaf469e43074daacc4f84d2c37e2a5ded7.
    
    * fix
    
    * Эта хуйня работает
    
    * Final fix
    
    ---------
    
    Co-authored-by: Ivan Rubinov <linuxkernelpatch8234@riseup.net>
    Co-authored-by: Ed <96445749+TheShuEd@users.noreply.github.com>

--- Content.Shared/Standing/StandingStateComponent.cs
+++ Content.Shared/Standing/StandingStateComponent.cs
@@ -1,24 +1,45 @@
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
+using Content.Shared.Actions;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
-namespace Content.Shared.Standing
+namespace Content.Shared.Standing;
+
+
+[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+[Access(typeof(StandingStateSystem))]
+public sealed partial class StandingStateComponent : Component
 {
-    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-    [Access(typeof(StandingStateSystem))]
-    public sealed partial class StandingStateComponent : Component
-    {
-        [ViewVariables(VVAccess.ReadWrite)]
-        [DataField]
-        public SoundSpecifier DownSound { get; private set; } = new SoundCollectionSpecifier("BodyFall");
+    [ViewVariables(VVAccess.ReadWrite)]
+    [DataField]
+    public SoundSpecifier DownSound { get; private set; } = new SoundCollectionSpecifier("BodyFall");
 
-        [DataField, AutoNetworkedField]
-        public bool Standing { get; set; } = true;
+    [DataField, AutoNetworkedField]
+    public bool Standing = true;
 
-        /// <summary>
-        ///     List of fixtures that had their collision mask changed when the entity was downed.
-        ///     Required for re-adding the collision mask.
-        /// </summary>
-        [DataField, AutoNetworkedField]
-        public List<string> ChangedFixtures = new();
-    }
+    /// <summary>
+    ///     List of fixtures that had their collision mask changed when the entity was downed.
+    ///     Required for re-adding the collision mask.
+    /// </summary>
+    [DataField, AutoNetworkedField]
+    public List<string> ChangedFixtures = new();
+
+    [DataField]
+    public EntProtoId LieDownAction = "ActionLieDown";
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? LieDownActionEntity;
+
+    [DataField("stand-up-action")]
+    public EntProtoId StandUpAction = "ActionStandUp";
+
+    [DataField, AutoNetworkedField]
+    public EntityUid? StandUpActionEntity;
 }
+
+public sealed partial class LieDownActionEvent : InstantActionEvent {}
+public sealed partial class StandUpActionEvent : InstantActionEvent {}
+
