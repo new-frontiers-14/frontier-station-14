45472db88bd58038dae8ace80720f72e8a685759
Kill_Me_I_Noobs <118206719+Vonsant@users.noreply.github.com>
2024-06-23 21:51:48 +0300

    Revert "Ляжания вторая попытка - ДОДЕЛАННОЕ, РАБОЧЕЕ ГАВНО (#325)"
    This reverts commit 1ac73bc45933a610f436c246aff6e72fce75d641.

--- Content.Client/Input/ContentContexts.cs
+++ Content.Client/Input/ContentContexts.cs
@@ -81,7 +81,6 @@ namespace Content.Client.Input
             human.AddFunction(ContentKeyFunctions.Arcade1);
             human.AddFunction(ContentKeyFunctions.Arcade2);
             human.AddFunction(ContentKeyFunctions.Arcade3);
-            human.AddFunction(ContentKeyFunctions.LieDownStandUp);
 
             // actions should be common (for ghosts, mobs, etc)
             common.AddFunction(ContentKeyFunctions.OpenActionsMenu);
--- Content.Shared/Standing/StandingStateComponent.cs
+++ Content.Shared/Standing/StandingStateComponent.cs
@@ -1,45 +1,24 @@
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Content.Shared.Actions;
-using Robust.Shared.Prototypes;
-using Robust.Shared.Serialization;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
-using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
-namespace Content.Shared.Standing;
-
-
-[RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
-[Access(typeof(StandingStateSystem))]
-public sealed partial class StandingStateComponent : Component
+namespace Content.Shared.Standing
 {
-    [ViewVariables(VVAccess.ReadWrite)]
-    [DataField]
-    public SoundSpecifier DownSound { get; private set; } = new SoundCollectionSpecifier("BodyFall");
+    [RegisterComponent, NetworkedComponent, AutoGenerateComponentState]
+    [Access(typeof(StandingStateSystem))]
+    public sealed partial class StandingStateComponent : Component
+    {
+        [ViewVariables(VVAccess.ReadWrite)]
+        [DataField]
+        public SoundSpecifier DownSound { get; private set; } = new SoundCollectionSpecifier("BodyFall");
 
-    [DataField, AutoNetworkedField]
-    public bool Standing = true;
+        [DataField, AutoNetworkedField]
+        public bool Standing { get; set; } = true;
 
-    /// <summary>
-    ///     List of fixtures that had their collision mask changed when the entity was downed.
-    ///     Required for re-adding the collision mask.
-    /// </summary>
-    [DataField, AutoNetworkedField]
-    public List<string> ChangedFixtures = new();
-
-    [DataField]
-    public EntProtoId LieDownAction = "ActionLieDown";
-
-    [DataField, AutoNetworkedField]
-    public EntityUid? LieDownActionEntity;
-
-    [DataField("stand-up-action")]
-    public EntProtoId StandUpAction = "ActionStandUp";
-
-    [DataField, AutoNetworkedField]
-    public EntityUid? StandUpActionEntity;
+        /// <summary>
+        ///     List of fixtures that had their collision mask changed when the entity was downed.
+        ///     Required for re-adding the collision mask.
+        /// </summary>
+        [DataField, AutoNetworkedField]
+        public List<string> ChangedFixtures = new();
+    }
 }
-
-public sealed partial class LieDownActionEvent : InstantActionEvent {}
-public sealed partial class StandUpActionEvent : InstantActionEvent {}
-
