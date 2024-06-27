4196b944cedc5b13088350f42fda869fbbe9cfc1
Whatstone <166147148+whatston3@users.noreply.github.com>
2024-06-27 13:24:47 -0400

    Remove placing items on tabletop boards. (#29513) (#1570)
    This feature should never have been merged, it can be trivially abused to break the entire server.
    
    It's behind a CVar because honestly that's the easiest way to 1984 the feature.
    
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers+git@gmail.com>

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -364,6 +364,17 @@ namespace Content.Shared.CCVar
         public static readonly CVarDef<string> RoundEndSoundCollection =
             CVarDef.Create("game.round_end_sound_collection", "RoundEnd", CVar.SERVERONLY);
 
+
+        /// <summary>
+        /// If true, players can place objects onto tabletop games like chess boards.
+        /// </summary>
+        /// <remarks>
+        /// This feature is currently highly abusable and can easily be used to crash the server,
+        /// so it's off by default.
+        /// </remarks>
+        public static readonly CVarDef<bool> GameTabletopPlace =
+            CVarDef.Create("game.tabletop_place", false, CVar.SERVERONLY);
+
         /*
          * Discord
          */
