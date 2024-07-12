c4e2f9fa111b9878e8607d7c3d3c12c3f66d88dc
Whatstone <whatston3@gmail.com>
2024-07-12 13:30:48 -0400

    Fix goblin accent

--- Content.Server/_NF/Speech/EntitySystems/GoblinAccentSystem.cs
+++ Content.Server/_NF/Speech/EntitySystems/GoblinAccentSystem.cs
@@ -8,14 +8,14 @@ namespace Content.Server._NF.Speech.EntitySystems;
 // The whole code is a copy of SouthernAccentSystem by UBlueberry (https://github.com/UBlueberry)
 public sealed class GoblinAccentSystem : EntitySystem
 {
-    private static readonly Regex RegexIng = new(@"ing\b");
-    private static readonly Regex RegexAnd = new(@"\band\b");
-    private static readonly Regex RegexEr = new(@"er\b");
-    private static readonly Regex RegexTt = new(@"tt");
-    private static readonly Regex RegexOf = new(@"\bof\b");
-    private static readonly Regex RegexThe = new(@"\bthe\b");
-    private static readonly Regex RegexH = new(@"\bh");
-    private static readonly Regex RegexSelf = new(@"self\b");
+    private static readonly Regex RegexIng = new(@"ing\b", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexAnd = new(@"\band\b", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexEr = new(@"er\b", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexTt = new(@"tt", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexOf = new(@"\bof\b", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexThe = new(@"\bthe\b", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexH = new(@"\bh", RegexOptions.IgnoreCase);
+    private static readonly Regex RegexSelf = new(@"self\b", RegexOptions.IgnoreCase);
 
     [Dependency] private readonly ReplacementAccentSystem _replacement = default!;
 
@@ -27,9 +27,12 @@ public sealed class GoblinAccentSystem : EntitySystem
 
     private void OnAccent(EntityUid uid, GoblinAccentComponent component, AccentGetEvent args)
     {
+        Log.Error($"OnAccent! {args.Message}");
         var message = args.Message;
 
-        message = _replacement.ApplyReplacements(message, "goblin");
+        message = _replacement.ApplyReplacements(message, "goblin_accent");
+
+        Log.Error(message);
 
         message = RegexIng.Replace(message, "in'");
         message = RegexAnd.Replace(message, "an'");
@@ -39,6 +42,8 @@ public sealed class GoblinAccentSystem : EntitySystem
         message = RegexSelf.Replace(message, "sewf");
         message = RegexOf.Replace(message, "o'");
         message = RegexThe.Replace(message, "da");
+
+        Log.Error(message);
 
         args.Message = message;
     }
