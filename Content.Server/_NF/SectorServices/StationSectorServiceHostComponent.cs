0879a1de281dc2c0beefa4b1e62e8fc5f6fd1f01
Whatstone <whatston3@gmail.com>
2024-07-17 08:38:01 -0400

    partial problems require partial components

--- Content.Server/_NF/SectorServices/StationSectorServiceHostComponent.cs
+++ Content.Server/_NF/SectorServices/StationSectorServiceHostComponent.cs
@@ -1,11 +1,9 @@
-using Robust.Shared.GameObjects;
-
 namespace Content.Server._NF.SectorServices;
 
 /// <summary>
 /// A station with this component will host all sector-wide services.
 /// </summary>
 [RegisterComponent]
-public sealed class StationSectorServiceHostComponent : Component
+public sealed partial class StationSectorServiceHostComponent : Component
 {
 }
