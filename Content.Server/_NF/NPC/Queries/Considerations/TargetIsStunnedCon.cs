a06eb19d09194b0fac6e3bca40fb1bd1cfe438e2
steinhauer.erhard <steinhauer.erhard@gmail.com>
2024-07-18 19:33:27 +0300

    stolen valor (from Whatstone)

---
+++ Content.Server/_NF/NPC/Queries/Considerations/TargetIsStunnedCon.cs
@@ -0,0 +1,9 @@
+namespace Content.Server.NPC.Queries.Considerations;
+
+/// <summary>
+/// Returns true if the target is stunned
+/// </summary>
+public sealed partial class TargetIsStunnedCon : UtilityConsideration
+{
+
+}
