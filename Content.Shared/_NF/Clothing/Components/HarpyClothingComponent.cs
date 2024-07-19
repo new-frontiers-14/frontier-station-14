c411e1f0df1f03619a5f97e73f65925840f7660f
Dvir <dvirf01@gmail.com>
2024-07-19 12:07:14 +0300

    No More Tag

---
+++ Content.Shared/_NF/Clothing/Components/HarpyClothingComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._NF.Clothing.Components;
+/// <summary>
+/// To be used with Harpy to replace the tag
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HarpyClothingComponent : Component
+{
+
+}
