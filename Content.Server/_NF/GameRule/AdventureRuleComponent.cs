da68b60b78dfe313de8bd2ad83778bce8350cedb
Whatstone <whatston3@gmail.com>
2024-07-29 17:22:59 -0400

    AdventureRuleComponent: restore members

--- Content.Server/_NF/GameRule/AdventureRuleComponent.cs
+++ Content.Server/_NF/GameRule/AdventureRuleComponent.cs
@@ -3,4 +3,10 @@ namespace Content.Server.GameTicking.Rules.Components;
 [RegisterComponent, Access(typeof(NfAdventureRuleSystem))]
 public sealed partial class AdventureRuleComponent : Component
 {
+    public readonly List<EntityUid> NFPlayerMinds = new();
+    public readonly List<EntityUid> CargoDepots = new();
+    public readonly List<EntityUid> MarketStations = new();
+    public readonly List<EntityUid> RequiredPOIs = new();
+    public readonly List<EntityUid> OptionalPOIs = new();
+    public readonly List<EntityUid> UniquePOIs = new();
 }
