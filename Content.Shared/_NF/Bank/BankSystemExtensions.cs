fa50e634334904393dc9100e4040dd58fe645893
Wiebe Geertsma <1354802+GreaseMonk@users.noreply.github.com>
2024-08-02 19:36:39 +0200

    Darin fix

--- Content.Shared/_NF/Bank/BankSystemExtensions.cs
+++ Content.Shared/_NF/Bank/BankSystemExtensions.cs
@@ -1,10 +1,33 @@
-﻿namespace Content.Shared._NF.Bank;
+﻿using System.Globalization;
+
+namespace Content.Shared._NF.Bank;
 
 public static class BankSystemExtensions
 {
-    public static string ToCurrencyString(int amount)
+    /// <summary>
+    /// Formats a integer to the current CultureInfo's number formatting for currency.
+    /// </summary>
+    /// <param name="amount">The amount to format</param>
+    /// <param name="culture">The optional culture to use for formatting</param>
+    /// <param name="symbolOverride">Optionally override the symbol</param>
+    /// <param name="separatorOverride">Optionally override the separator</param>
+    /// <returns></returns>
+    public static string ToCurrencyString(int amount, CultureInfo? culture = null, string? symbolOverride = null, string? separatorOverride = null)
     {
-        return $"${amount:N0}".Replace(",", ".");
+        culture ??= CultureInfo.CurrentCulture;
+        var numberFormat = (NumberFormatInfo)culture.NumberFormat.Clone();
+
+        if (symbolOverride != null)
+        {
+            numberFormat.CurrencySymbol = symbolOverride;
+        }
+        if (separatorOverride != null)
+        {
+            numberFormat.CurrencyGroupSeparator = separatorOverride;
+        }
+
+
+        return string.Format(numberFormat, "{0:C0}", amount);
     }
 }
 
