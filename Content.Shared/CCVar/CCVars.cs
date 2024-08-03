bda9b6611250a6dcfa0c344ad5d3361395ac1011
VMSolidus <evilexecutive@gmail.com>
2024-08-02 20:41:53 -0400

    Supermatter Engine (Port From White Dream) (#341)
    ![SM
    boom](https://github.com/Simple-Station/Einstein-Engines/assets/16548818/6937c520-0963-437c-a9a6-49afe6c8b787)
    
    ![New edge
    supermatter](https://github.com/Simple-Station/Einstein-Engines/assets/16548818/e8305536-b777-41f8-b9bf-295d402dc1bf)
    
    This PR has been produced in collaboration with coders from White Dream,
    with written permission given by the relevant code owners to port this
    specific slice of content to Einstein-Engines. Supermatter Engines are a
    form of nuclear reactor, which produces energy in the form of
    radioactive particles, while also decaying into Phoron when excited by
    an external energy source.
    
    Power can be obtained from the engine via radiation collectors, which
    like those of a singularity engine, must be periodically refueled. In
    addition, the engine must also be actively cooled via aid from
    Atmospherics, and for undesirable gasses to be extracted from the
    reactor chamber.
    
    If not cooled, the crystal will begin to destabilize and eventually
    collapse into one of three different situations depending on the source
    of its instability.
    1. A nuclear blast.
    2. A gravitational singularity
    3. A Tesla ball
    
    :cl: VMSolidus, White Dream, Colin-Tel
    - add: Supermatter Engines have been implemented.
    
    ---------
    
    Signed-off-by: VMSolidus <evilexecutive@gmail.com>
    Co-authored-by: whateverusername0 <whateveremail>
    Co-authored-by: username <113782077+whateverusername0@users.noreply.github.com>
    Co-authored-by: Danger Revolution! <142105406+DangerRevolution@users.noreply.github.com>
    Co-authored-by: Pieter-Jan Briers <pieterjan.briers@gmail.com>
    Co-authored-by: DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>

--- Content.Shared/CCVar/CCVars.cs
+++ Content.Shared/CCVar/CCVars.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Maps;
 using Content.Shared.Roles;
+using Content.Shared.Supermatter.Components;
 using Robust.Shared;
 using Robust.Shared.Configuration;
 using Robust.Shared.Physics.Components;
@@ -2272,5 +2273,55 @@ namespace Content.Shared.CCVar
         /// </summary>
         public static readonly CVarDef<bool> DebugPow3rDisableParallel =
             CVarDef.Create("debug.pow3r_disable_parallel", true, CVar.SERVERONLY);
+
+
+        #region Supermatter System
+
+        /// <summary>
+        ///     With completely default supermatter values, Singuloose delamination will occur if engineers inject at least 900 moles of coolant per tile
+        ///     in the crystal chamber. For reference, a gas canister contains 1800 moles of air. This Cvar directly multiplies the amount of moles required to singuloose.
+        /// </summary>
+        public static readonly CVarDef<float> SupermatterSingulooseMolesModifier =
+            CVarDef.Create("supermatter.singuloose_moles_modifier", 1f, CVar.SERVER);
+
+        /// <summary>
+        ///     Toggles whether or not Singuloose delaminations can occur. If both Singuloose and Tesloose are disabled, it will always delam into a Nuke.
+        /// </summary>
+        public static readonly CVarDef<bool> SupermatterDoSingulooseDelam =
+            CVarDef.Create("supermatter.do_singuloose", true, CVar.SERVER);
+
+        /// <summary>
+        ///     By default, Supermatter will "Tesloose" if the conditions for Singuloose are not met, and the core's power is at least 4000.
+        ///     The actual reasons for being at least this amount vary by how the core was screwed up, but traditionally it's caused by "The core is on fire".
+        ///     This Cvar multiplies said power threshold for the purpose of determining if the delam is a Tesloose.
+        /// </summary>
+        public static readonly CVarDef<float> SupermatterTesloosePowerModifier =
+            CVarDef.Create("supermatter.tesloose_power_modifier", 1f, CVar.SERVER);
+
+        /// <summary>
+        ///     Toggles whether or not Tesloose delaminations can occur. If both Singuloose and Tesloose are disabled, it will always delam into a Nuke.
+        /// </summary>
+        public static readonly CVarDef<bool> SupermatterDoTeslooseDelam =
+            CVarDef.Create("supermatter.do_tesloose", true, CVar.SERVER);
+
+        /// <summary>
+        ///     When true, bypass the normal checks to determine delam type, and instead use the type chosen by supermatter.forced_delam_type
+        /// </summary>
+        public static readonly CVarDef<bool> SupermatterDoForceDelam =
+            CVarDef.Create("supermatter.do_force_delam", false, CVar.SERVER);
+
+        /// <summary>
+        ///     If supermatter.do_force_delam is true, this determines the delamination type, bypassing the normal checks.
+        /// </summary>
+        public static readonly CVarDef<DelamType> SupermatterForcedDelamType =
+            CVarDef.Create("supermatter.forced_delam_type", DelamType.Singulo, CVar.SERVER);
+
+        /// <summary>
+        ///     Directly multiplies the amount of rads put out by the supermatter. Be VERY conservative with this.
+        /// </summary>
+        public static readonly CVarDef<float> SupermatterRadsModifier =
+            CVarDef.Create("supermatter.rads_modifier", 1f, CVar.SERVER);
+
+        #endregion
     }
 }
---
+++ Content.Shared/Supermatter/Components/SupermatterImmuneComponent.cs
@@ -0,0 +1,9 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Supermatter.Components;
+
+[RegisterComponent, NetworkedComponent]
+public sealed partial class SupermatterImmuneComponent : Component
+{
+
+}
