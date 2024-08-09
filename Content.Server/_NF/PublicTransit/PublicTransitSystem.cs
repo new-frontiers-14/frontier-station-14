f03b35cc5045b3aeb5dd915f30440161b4cf799c
Whatstone <whatston3@gmail.com>
2024-08-09 11:03:04 -0400

    PublicTransitSystem: use priority dock tag

--- Content.Server/_NF/PublicTransit/PublicTransitSystem.cs
+++ Content.Server/_NF/PublicTransit/PublicTransitSystem.cs
@@ -201,9 +201,9 @@ public sealed class PublicTransitSystem : EntitySystem
                         ignoreActionBlocker: true);
                 }
             }
-            _shuttles.FTLToDock(uid, shuttle, comp.NextStation, hyperspaceTime: FlyTime);
+            _shuttles.FTLToDock(uid, shuttle, comp.NextStation, hyperspaceTime: FlyTime, priorityTag: "DockTransit");
 
-            if (TryGetNextStation(out var nextStation) && nextStation is {Valid : true} destination)
+            if (TryGetNextStation(out var nextStation) && nextStation is { Valid: true } destination)
                 comp.NextStation = destination;
 
             comp.NextTransfer = curTime + TimeSpan.FromSeconds(FlyTime + _cfgManager.GetCVar(NF14CVars.PublicTransitWaitTime));
