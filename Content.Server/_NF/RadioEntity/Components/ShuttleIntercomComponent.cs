8c7a4f647c33888b53b2b6c490d8d1afe0b4f83a
Dvir <dvirf01@gmail.com>
2024-08-16 22:00:59 +0300

    IntercomStarting PR

---
+++ Content.Server/_NF/RadioEntity/Components/ShuttleIntercomComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared.Radio.Components;
+
+/// <summary>
+/// Handles intercom ui and is authoritative on the channels an intercom can access.
+/// </summary>
+[RegisterComponent]
+public sealed partial class ShuttleIntercomComponent : Component
+{
+}
---
+++ Content.Server/_NF/RadioEntity/Systems/ShuttleIntercomSystem.cs
@@ -0,0 +1,56 @@
+using Content.Shared.Radio.Components;
+using Robust.Server.GameObjects;
+using Content.Shared.Verbs;
+using Robust.Shared.Player;
+using Content.Shared.Radio;
+
+namespace Content.Server.Radio.EntitySystems;
+
+/// <summary>
+///     Skibidi Code?
+/// </summary>
+public sealed partial class ShuttleIntercomSystem : EntitySystem
+{
+    [Dependency] private readonly UserInterfaceSystem _ui = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<ShuttleIntercomComponent, GetVerbsEvent<AlternativeVerb>>(OnAlternativeVerb);
+    }
+
+    private void OnAlternativeVerb(EntityUid uid, ShuttleIntercomComponent component, GetVerbsEvent<AlternativeVerb> args)
+    {
+        if (!args.CanAccess || !args.CanInteract)
+            return;
+
+        var openUiVerb = new AlternativeVerb
+        {
+            Act = () => ToggleUi(uid, component, args.User),
+            Text = Loc.GetString("mech-ui-open-verb")
+        };
+        args.Verbs.Add(openUiVerb);
+    }
+
+    private void ToggleUi(EntityUid uid, ShuttleIntercomComponent? component = null, EntityUid? user = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        if (!TryComp<ActorComponent>(user, out var actor))
+            return;
+
+        _ui.TryToggleUi(uid, IntercomUiKey.Key, actor.PlayerSession);
+        UpdateUserInterface(uid, component);
+    }
+
+    public override void UpdateUserInterface(EntityUid uid, ShuttleIntercomComponent? component = null)
+    {
+        if (!Resolve(uid, ref component))
+            return;
+
+        var state = new IntercomBoundUIState(micEnabled, speakerEnabled, availableChannels, selectedChannel);
+
+        _ui.SetUiState(uid, IntercomUiKey.Key, state);
+    }
+}
