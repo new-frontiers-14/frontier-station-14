2b7039fc7c7f24cea51c930a0c3d736e540de3cd
Whatstone <166147148+whatston3@users.noreply.github.com>
2024-08-18 19:40:11 -0400

    Mail Metrics: access from anywhere, consistent error tracking. (#1885)
    * mail sector service, log only first failure
    
    * MailSystem: missed damaged check
    
    * Update mail.yml
    
    * Update pda.yml
    
    * Frontier: better(?) comments on MailMetrics chngs
    
    ---------
    
    Co-authored-by: Dvir <dvirf01@gmail.com>

--- Content.Server/DeltaV/CartridgeLoader/Cartridges/MailMetricsCartridgeComponent.cs
+++ Content.Server/DeltaV/CartridgeLoader/Cartridges/MailMetricsCartridgeComponent.cs
@@ -1,11 +1,7 @@
 namespace Content.Server.DeltaV.CartridgeLoader.Cartridges;
 
+// Frontier: removed the EntityUid of the station from the component 
 [RegisterComponent, Access(typeof(MailMetricsCartridgeSystem))]
 public sealed partial class MailMetricsCartridgeComponent : Component
 {
-    /// <summary>
-    /// Station entity keeping track of logistics stats
-    /// </summary>
-    [DataField]
-    public EntityUid? Station;
 }
