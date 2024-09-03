1a4eb2b939ad22c71bbdea9303324ea2feaaa48f
Dvir <dvirf01@gmail.com>
2024-09-03 19:48:32 +0300

    Fixup

--- Content.Server/_NF/Tools/Component/OnToolsUseComponent.cs
+++ Content.Server/_NF/Tools/Component/OnToolsUseComponent.cs
@@ -7,15 +7,9 @@ namespace Content.Server._NF.Tools.Components
     public sealed partial class OnToolsUseComponent : Component
     {
         /// <summary>
-        ///     Disables the use of all tools on an entity
-        /// </summary>
-        [DataField("disabled"), ViewVariables(VVAccess.ReadWrite)]
-        public bool AllToolUseDisabled;
-
-        /// <summary>
         ///     Disable the use of tools on the entity.
         /// </summary>
         [DataField, ViewVariables(VVAccess.ReadWrite)]
-        public PrototypeFlags<ToolQualityPrototype> DisabledQualities = [];
+        public PrototypeFlags<ToolQualityPrototype> DisabledQualities = new(["Anchoring", "Prying", "Screwing", "Cutting", "Welding", "Pulsing", "Slicing", "Sawing", "Honking", "Rolling", "Digging"]);
     }
 }
