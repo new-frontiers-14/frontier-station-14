118ab09d08b798d8510ee62dd9d5d0b7ccd7fd4f
Whatstone <166147148+whatston3@users.noreply.github.com>
2024-09-27 15:50:34 -0400

    Expeditions: teardown resources on failures to generate. (#2063)
    * Teardown resources on failing expedition gen
    
    * Map Cleanup
    
    ---------
    
    Co-authored-by: Dvir <39403717+dvir001@users.noreply.github.com>
    Co-authored-by: Dvir <dvirf01@gmail.com>

--- Content.Server/Salvage/SalvageSystem.Runner.cs
+++ Content.Server/Salvage/SalvageSystem.Runner.cs
@@ -107,7 +107,7 @@ public sealed partial class SalvageSystem
         if (TryComp<SalvageExpeditionDataComponent>(component.Station, out var data))
         {
             data.CanFinish = true;
-            UpdateConsoles(data);
+            UpdateConsoles(component.Station, data);
         }
         // Frontier
 
@@ -117,13 +117,13 @@ public sealed partial class SalvageSystem
 
         Announce(args.MapUid, Loc.GetString("salvage-expedition-announcement-countdown-minutes", ("duration", (component.EndTime - _timing.CurTime).Minutes)));
 
-         var directionLocalization = ContentLocalizationManager.FormatDirection(component.DungeonLocation.GetDir()).ToLower();
+        var directionLocalization = ContentLocalizationManager.FormatDirection(component.DungeonLocation.GetDir()).ToLower();
 
         if (component.DungeonLocation != Vector2.Zero)
             Announce(args.MapUid, Loc.GetString("salvage-expedition-announcement-dungeon", ("direction", directionLocalization)));
 
         component.Stage = ExpeditionStage.Running;
-        Dirty(component);
+        Dirty(args.MapUid, component);
     }
 
     private void OnFTLStarted(ref FTLStartedEvent ev)
