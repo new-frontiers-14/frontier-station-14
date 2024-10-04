cd3301bcf86af4c751d6784e0435620d6bef7cd5
Dmitriy <108795395+Sh1ntra@users.noreply.github.com>
2024-10-04 17:21:03 +0300

    try fix publish

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -14,7 +14,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "https://wizards.cdn.spacestation14.com/"
+ROBUST_CDN_URL = "https://cdn.station14.ru/"
 FORK_ID = os.environ["FORK_ID"]
 
 def main():
