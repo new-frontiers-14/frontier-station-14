c3981c865c6481fdb0af01beafe6b5a7adfd2871
GreaseMonk <1354802+GreaseMonk@users.noreply.github.com>
2024-10-13 18:54:29 +0200

    Fix issues

--- Content.Shared/_NF/ShuttleRecords/SharedShuttleRecordsSystem.cs
+++ Content.Shared/_NF/ShuttleRecords/SharedShuttleRecordsSystem.cs
@@ -1,5 +1,7 @@
-﻿using Content.Shared.Containers.ItemSlots;
+﻿using Content.Shared._NF.ShuttleRecords.Components;
+using Content.Shared.Containers.ItemSlots;
 using Robust.Shared.Audio.Systems;
+using Robust.Shared.Serialization;
 
 namespace Content.Shared._NF.ShuttleRecords;
 
@@ -8,4 +10,21 @@ public abstract class SharedShuttleRecordsSystem : EntitySystem
     // These dependencies are eventually needed for the consoles that are made for this system.
     [Dependency] protected readonly ItemSlotsSystem _itemSlotsSystem = default!;
     [Dependency] protected readonly SharedAudioSystem _audioSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<ShuttleRecordsConsoleComponent, ComponentInit>(OnComponentInit);
+    }
+
+    private void OnComponentInit(EntityUid uid, ShuttleRecordsConsoleComponent component, ComponentInit args)
+    {
+        _itemSlotsSystem.AddItemSlot(uid, ShuttleRecordsConsoleComponent.TargetIdCardSlotId, component.TargetIdSlot);
+    }
+}
+
+[NetSerializable, Serializable]
+public enum ShuttleRecordsUiKey : byte
+{
+    Default,
 }
