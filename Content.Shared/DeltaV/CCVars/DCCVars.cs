0531b13a791328e07c55ecf2aabaeea8f786e6a6
Dvir <39403717+dvir001@users.noreply.github.com>
2024-10-25 23:56:41 +0300

    Update DCCVars.cs

--- Content.Shared/DeltaV/CCVars/DCCVars.cs
+++ Content.Shared/DeltaV/CCVars/DCCVars.cs
@@ -14,7 +14,7 @@ public sealed class DCCVars
     /// Its not perfect, but gets the job done.
     /// </summary>
     public static readonly CVarDef<bool> RoundEndPacifist =
-        CVarDef.Create("game.round_end_pacifist", true, CVar.SERVERONLY);
+        CVarDef.Create("game.round_end_pacifist", false, CVar.REPLICATED);
 
     /// <summary>
     /// Disables all vision filters for species like Vulpkanin or Harpies. There are good reasons someone might want to disable these.
