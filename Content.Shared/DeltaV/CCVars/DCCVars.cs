24d4d861f8a0ffd2976abb4d0f3faed54b6ba2f0
Milon <milonpl.git@proton.me>
2024-10-29 00:36:49 +0100

    add the no EORG popup (#2070)
    ONE MORE PR TODAY

--- Content.Shared/DeltaV/CCVars/DCCVars.cs
+++ Content.Shared/DeltaV/CCVars/DCCVars.cs
@@ -17,6 +17,24 @@ public sealed class DCCVars
         CVarDef.Create("game.round_end_pacifist", false, CVar.REPLICATED);
 
     /// <summary>
+    /// Whether the no EORG popup is enabled.
+    /// </summary>
+    public static readonly CVarDef<bool> RoundEndNoEorgPopup =
+        CVarDef.Create("game.round_end_eorg_popup_enabled", true, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
+    /// Skip the no EORG popup.
+    /// </summary>
+    public static readonly CVarDef<bool> SkipRoundEndNoEorgPopup =
+        CVarDef.Create("game.skip_round_end_eorg_popup", false, CVar.CLIENTONLY | CVar.ARCHIVE);
+
+    /// <summary>
+    /// How long to display the EORG popup for.
+    /// </summary>
+    public static readonly CVarDef<float> RoundEndNoEorgPopupTime =
+        CVarDef.Create("game.round_end_eorg_popup_time", 5f, CVar.SERVER | CVar.REPLICATED);
+
+    /// <summary>
     /// Disables all vision filters for species like Vulpkanin or Harpies. There are good reasons someone might want to disable these.
     /// </summary>
     public static readonly CVarDef<bool> NoVisionFilters =
---
+++ Resources/Locale/en-US/deltav/roundend/no-eorg-popup.ftl
@@ -0,0 +1,8 @@
+no-eorg-popup-title = Delta-V
+no-eorg-popup-label = Welcome to the End of Round!
+no-eorg-popup-message = [bold]End-of-round grief (EORG)[/bold] is not allowed at Delta-V. Please stay in character until the lobby screen appears to maintain an immersive environment for everyone. Thank you for respecting the community rules!
+no-eorg-popup-rule = [bold][color=#a4885c]Rule B4: Significant end-of-round grief (EORG) is not allowed.[/color][/bold]
+no-eorg-popup-rule-text = This includes attacking, destroying, polluting, and severely injuring without reason both at and on the way to Central Command. Remember that you are playing a character throughout the round.
+no-eorg-popup-close-button = Sounds good!
+no-eorg-popup-close-button-wait = The close button will be enabled after {$time} seconds.
+no-eorg-popup-skip-checkbox = Don't show this again.
