f917167981f20cf001ba34dc61c5eea1e92d5699
Whatstone <whatston3@gmail.com>
2024-12-01 12:10:10 -0500

    POC for price reduction, contraband value removal

--- Content.Server/Lathe/LatheSystem.cs
+++ Content.Server/Lathe/LatheSystem.cs
@@ -30,6 +30,8 @@ using Robust.Server.GameObjects;
 using Robust.Shared.Audio.Systems;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Timing;
+using Content.Shared.Cargo.Components;
+using Content.Shared.Contraband;
 
 namespace Content.Server.Lathe
 {
@@ -234,6 +236,37 @@ namespace Content.Server.Lathe
                 if (comp.CurrentRecipe.Result is { } resultProto)
                 {
                     var result = Spawn(resultProto, Transform(uid).Coordinates);
+
+                    // Frontier: adjust price before merge (stack prices changed once)
+                    if (result.Valid)
+                    {
+                        if (comp.ProductValueModifier != null
+                        && float.IsFinite(comp.ProductValueModifier.Value)
+                        && comp.ProductValueModifier >= 0f)
+                        {
+                            if (TryComp<StackPriceComponent>(result, out var stackPrice))
+                            {
+                                if (stackPrice.Price > 0)
+                                    stackPrice.Price *= comp.ProductValueModifier.Value;
+                            }
+                            if (TryComp<StaticPriceComponent>(result, out var staticPrice))
+                            {
+                                if (staticPrice.Price > 0)
+                                    staticPrice.Price *= comp.ProductValueModifier.Value;
+                            }
+                        }
+
+                        // Clear contraband value for printed items
+                        if (TryComp<ContrabandComponent>(result, out var contraband))
+                        {
+                            foreach (var valueKey in contraband.TurnInValues.Keys)
+                            {
+                                contraband.TurnInValues[valueKey] = 0;
+                            }
+                        }
+                    }
+                    // End Frontier
+
                     _stack.TryMergeToContacts(result);
                 }
 
