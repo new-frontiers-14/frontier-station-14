b8c0456ada7312594987930ac8706afeeed58337
Whatstone <whatston3@gmail.com>
2024-12-13 14:39:11 -0500

    Contraband: recurse into containers

--- Content.Shared/_NF/Contraband/SharedContrabandTurnInSystem.cs
+++ Content.Shared/_NF/Contraband/SharedContrabandTurnInSystem.cs
@@ -1,4 +1,5 @@
 using Content.Shared.Contraband;
+using Robust.Shared.Containers;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared._NF.Contraband;
@@ -19,6 +20,18 @@ public abstract class SharedContrabandTurnInSystem : EntitySystem
             foreach (var valueKey in contraband.TurnInValues.Keys)
             {
                 contraband.TurnInValues[valueKey] = 0;
+            }
+        }
+
+        // Recurse into contained entities
+        if (TryComp<ContainerManagerComponent>(item, out var containers))
+        {
+            foreach (var container in containers.Containers.Values)
+            {
+                foreach (var ent in container.ContainedEntities)
+                {
+                    ClearContrabandValue(ent);
+                }
             }
         }
     }
