f3d1cb697ca55595bbb9eb0abc503f88239c1080
Whatstone <whatston3@gmail.com>
2024-12-14 22:14:45 -0500

    Better fuel-grade conversion, sprites

---
+++ Content.Server/_NF/Power/Generator/FuelGradeAdapterComponent.cs
@@ -0,0 +1,17 @@
+using Content.Shared.Materials;
+using Robust.Shared.Prototypes; // Frontier
+
+namespace Content.Server._NF.Power.Generator;
+
+[RegisterComponent]
+public sealed partial class FuelGradeAdapterComponent : Component
+{
+    [DataField]
+    public ProtoId<MaterialPrototype> InputMaterial = "Plasma";
+
+    [DataField]
+    public ProtoId<MaterialPrototype> OutputMaterial = "FuelGradePlasma";
+
+    [DataField]
+    public float ConversionRate = 1.0f;
+}
