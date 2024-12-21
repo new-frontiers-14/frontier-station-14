a331c4e85077b5160e2114501d56ae40c8e359dc
Dvir <dvirf01@gmail.com>
2024-12-21 23:22:01 +0200

    Update RMCCVars.cs

--- Content.Shared/_RMC14/CCVar/RMCCVars.cs
+++ Content.Shared/_RMC14/CCVar/RMCCVars.cs
@@ -1,17 +1,16 @@
 using Robust.Shared.Configuration;
 
-namespace Content.Shared._RMC14.CCVar
+namespace Content.Shared._RMC14.CCVar;
+
+[CVarDefs]
+public sealed class RMCCVars
 {
-    [CVarDefs]
-    public sealed class RMCCVars
-    {
-        public static readonly CVarDef<float> RMCMentorHelpRateLimitPeriod =
-            CVarDef.Create("rmc.mentor_help_rate_limit_period", 2f, CVar.SERVERONLY);
+    public static readonly CVarDef<float> RMCMentorHelpRateLimitPeriod =
+        CVarDef.Create("rmc.mentor_help_rate_limit_period", 2f, CVar.SERVERONLY);
 
-        public static readonly CVarDef<int> RMCMentorHelpRateLimitCount =
-            CVarDef.Create("rmc.mentor_help_rate_limit_count", 10, CVar.SERVERONLY);
+    public static readonly CVarDef<int> RMCMentorHelpRateLimitCount =
+        CVarDef.Create("rmc.mentor_help_rate_limit_count", 10, CVar.SERVERONLY);
 
-        public static readonly CVarDef<string> RMCMentorHelpSound =
-            CVarDef.Create("rmc.mentor_help_sound", "/Audio/_RMC14/Effects/Admin/mhelp.ogg", CVar.ARCHIVE | CVar.CLIENTONLY);
-    }
+    public static readonly CVarDef<string> RMCMentorHelpSound =
+        CVarDef.Create("rmc.mentor_help_sound", "/Audio/_RMC14/Effects/Admin/mhelp.ogg", CVar.ARCHIVE | CVar.CLIENTONLY);
 }
