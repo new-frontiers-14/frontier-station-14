5bf8bedffb3e231de89019af2d3ba17fc61f0288
Alkheemist <alkheemist@gmail.com>
2025-01-03 10:04:04 +1100

    Add alt verb to toggle accents from clothing

--- Content.Server/Speech/EntitySystems/AddAccentClothingSystem.cs
+++ Content.Server/Speech/EntitySystems/AddAccentClothingSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server.Speech.Components;
 using Content.Shared.Clothing;
+using Content.Shared.Verbs; // Frontier
 
 namespace Content.Server.Speech.EntitySystems;
 
@@ -12,6 +13,7 @@ public sealed class AddAccentClothingSystem : EntitySystem
         base.Initialize();
         SubscribeLocalEvent<AddAccentClothingComponent, ClothingGotEquippedEvent>(OnGotEquipped);
         SubscribeLocalEvent<AddAccentClothingComponent, ClothingGotUnequippedEvent>(OnGotUnequipped);
+        SubscribeLocalEvent<AddAccentClothingComponent, GetVerbsEvent<AlternativeVerb>>(OnGetAltVerbs); // Frontier
     }
 
     private void OnGotEquipped(EntityUid uid, AddAccentClothingComponent component, ref ClothingGotEquippedEvent args)
@@ -22,7 +24,7 @@ public sealed class AddAccentClothingSystem : EntitySystem
             return;
 
         // add accent to the user
-        var accentComponent = (Component) _componentFactory.GetComponent(componentType);
+        var accentComponent = (Component)_componentFactory.GetComponent(componentType);
         AddComp(args.Wearer, accentComponent);
 
         // snowflake case for replacement accent
@@ -30,10 +32,12 @@ public sealed class AddAccentClothingSystem : EntitySystem
             rep.Accent = component.ReplacementPrototype!;
 
         component.IsActive = true;
+        component.Wearer = args.Wearer;
     }
 
     private void OnGotUnequipped(EntityUid uid, AddAccentClothingComponent component, ref ClothingGotUnequippedEvent args)
     {
+        component.Wearer = new EntityUid(0); // null out the component wearer entry to prevent alt verb interactions when no longer worn. Frontier
         if (!component.IsActive)
             return;
 
@@ -46,4 +50,55 @@ public sealed class AddAccentClothingSystem : EntitySystem
 
         component.IsActive = false;
     }
+
+    // Frontier Start
+    /// <summary>
+    ///     Adds an alt verb allowing for the accent to be toggled easily.
+    /// </summary>
+    private void OnGetAltVerbs(EntityUid uid, AddAccentClothingComponent component, GetVerbsEvent<AlternativeVerb> args)
+    {
+        if (!args.CanInteract || args.User != component.Wearer) //only the wearer can toggle the effect
+            return;
+
+        AlternativeVerb verb = new()
+        {
+            Text = Loc.GetString("accent-clothing-component-toggle"),
+            Act = () => ToggleAccent(uid, component)
+        };
+        args.Verbs.Add(verb);
+    }
+
+    private void ToggleAccent(EntityUid uid, AddAccentClothingComponent component)
+    {
+        if (component.IsActive)
+        {
+            // try to remove the accent if it's enabled
+            var componentType = _componentFactory.GetRegistration(component.Accent).Type;
+            if (EntityManager.HasComponent(component.Wearer, componentType))
+            {
+                EntityManager.RemoveComponent(component.Wearer, componentType);
+            }
+            component.IsActive = false;
+            // we don't wipe out wearer in this case
+        }
+        else
+        {
+            // try to add the accent as if we are equipping this item again
+            // does the user already has this accent?
+            var componentType = _componentFactory.GetRegistration(component.Accent).Type;
+            if (HasComp(component.Wearer, componentType))
+                return;
+
+            // add accent to the user
+            var accentComponent = (Component)_componentFactory.GetComponent(componentType);
+            AddComp(component.Wearer, accentComponent);
+
+            // snowflake case for replacement accent
+            if (accentComponent is ReplacementAccentComponent rep)
+                rep.Accent = component.ReplacementPrototype!;
+
+            component.IsActive = true;
+        }
+    }
+    // Frontier End
 }
