7672df055a964430ad1564cae14b4adc4b9ac6f5
Dvir <dvirf01@gmail.com>
2025-01-13 01:48:34 +0200

    DeltaV

--- Content.Shared/DeltaV/TapeRecorder/Components/ActiveTapeRecorderComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.DeltaV.TapeRecorder.Components;
-
-/// <summary>
-/// Added to tape records that are updating, winding or rewinding the tape.
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class ActiveTapeRecorderComponent : Component;
--- Content.Shared/DeltaV/TapeRecorder/Components/FitsInTapeRecorderComponent.cs
+++
@@ -1,9 +0,0 @@
-using Robust.Shared.GameStates;
-
-namespace Content.Shared.DeltaV.TapeRecorder.Components;
-
-/// <summary>
-/// Removed from the cassette when damaged to prevent it being played until repaired
-/// </summary>
-[RegisterComponent, NetworkedComponent]
-public sealed partial class FitsInTapeRecorderComponent : Component;
