50687ec39e20dc89fad47ae86ce99b78c9fba52e
Alkheemist <alkheemist@gmail.com>
2025-01-13 23:50:33 +1100

    Revert "serverside recursive dock list method"
    This reverts commit 5880a60c5726587ebdef2a959ed3ca7b81369020.

--- Content.Server/_NF/Shuttles/Components/DockPassthroughComponent.cs
+++
@@ -1,9 +0,0 @@
-namespace Content.Server._NF.Shuttles.Components;
-
-/// <summary>
-///     Grids with this component will allow their docks to be controlled by any shuttle console on a grid docked to it, recursively.
-/// </summary>
-[RegisterComponent]
-public sealed partial class DockPassthroughComponent : Component
-{
-}
