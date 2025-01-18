9179627c11aaa9f8741171af795c526de312ab1a
Whatstone <whatston3@gmail.com>
2025-01-18 11:34:33 -0500

    gas sale point screen, remove unused console

--- Content.Server/_NF/Atmos/Components/GaslockConsoleComponent.cs
+++
@@ -1,6 +0,0 @@
-using Content.Shared._NF.Atmos.Components;
-
-namespace Content.Server._NF.Atmos.Components;
-
-[RegisterComponent]
-public sealed partial class GaslockConsoleComponent : SharedGaslockConsoleComponent;
