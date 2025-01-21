7f12f1390572b161ec11ece5e9f1d0426a4651d9
Milon <milonpl.git@proton.me>
2025-01-21 09:07:17 +0100

    offshore gas mining refactor (#2750)
    * headache inducing
    
    * Most PR suggestions
    
    * Random vs. regular GasDeposit, "gas" in scanner
    
    * unanchor port. gaslocks/mining drills, dir appear
    
    * predict deposit popup, dynamic gas structures
    
    ---------
    
    Co-authored-by: Whatstone <whatston3@gmail.com>

--- Content.Server/_NF/Worldgen/Components/Debris/RandomEntityPopulatorComponent.cs
+++ Content.Server/_NF/Worldgen/Components/Debris/RandomEntityPopulatorComponent.cs
@@ -8,8 +8,7 @@ namespace Content.Server.Worldgen.Components.Debris;
 /// <summary>
 ///     This is used for populating a grid with random entities automatically.
 /// </summary>
-[RegisterComponent]
-[Access(typeof(RandomEntityPopulatorSystem))]
+[RegisterComponent, Access(typeof(RandomEntityPopulatorSystem))]
 public sealed partial class RandomEntityPopulatorComponent : Component
 {
     private List<(RandomEntityParameters Params, EntitySpawnCollectionCache Cache)>? _caches;
@@ -24,38 +23,40 @@ public sealed partial class RandomEntityPopulatorComponent : Component
     ///     The spawn collections used to place entities on different tile types.
     /// </summary>
     [ViewVariables]
-    public List<(RandomEntityParameters Params, EntitySpawnCollectionCache Cache)> Caches
-    {
-        get
-        {
-            if (_caches is null)
-            {
-                _caches = _entries
-                    .Select(x => (x.Params, new EntitySpawnCollectionCache(x.Entries)))
-                    .ToList();
-            }
-
-            return _caches;
-        }
-    }
+    public List<(RandomEntityParameters Params, EntitySpawnCollectionCache Cache)> Caches =>
+        _caches ??= _entries
+            .Select(x => (x.Params, new EntitySpawnCollectionCache(x.Entries)))
+            .ToList();
 }
 
 // A random set of entities to spawn
 [DataDefinition]
 public sealed partial class RandomEntityParameters
 {
-    // The minimum number of this entity to spawn.
-    // Actual number is generated in a uniform range between min and max.
-    // Each entity is independently selected from the entity list below.
+    /// <summary>
+    /// The minimum number of this entity to spawn.
+    /// Actual number is generated in a uniform range between min and max.
+    /// Each entity is independently selected from the entity list below.
+    /// </summary>
     [DataField]
     public int Min = 1;
-    // The minimum number of this entity to spawn.
-    // Actual number is generated in a uniform range.
+
+    /// <summary>
+    /// The maximum number of this entity to spawn.
+    /// Actual number is generated in a uniform range.
+    /// </summary>
     [DataField]
     public int Max = 1;
+
+    /// <summary>
+    /// If true, this entity set will be spawned when air sealed (e.g. under a wall).
+    /// </summary>
     [DataField]
-    public bool CanBeAirSealed = false;
-    // The probability to generate this set of entities.
+    public bool CanBeAirSealed;
+
+    /// <summary>
+    /// The probability to generate this set of entities.
+    /// </summary>
     [DataField]
     public float Prob = 1.0f;
 }
@@ -65,6 +66,7 @@ public sealed partial class RandomEntityEntrySet
 {
     [DataField]
     public RandomEntityParameters Params;
+
     [DataField]
     public List<EntitySpawnEntry> Entries;
 }
