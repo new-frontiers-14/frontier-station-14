40761001987abbede4a378bac8071ae3e58e932f
Whatstone <whatston3@gmail.com>
2025-01-21 12:16:35 -0500

    marginally better plural comment

--- Content.Server/Silicons/Laws/IonStormSystem.cs
+++ Content.Server/Silicons/Laws/IonStormSystem.cs
@@ -174,7 +174,7 @@ public sealed class IonStormSystem : EntitySystem
         var number = Pick(NumberBase) + " " + Pick(NumberMod);
         var area = Pick(Areas);
         var feeling = Pick(FeelingsPlural); // Frontier: Feelings < FeelingsPlural (not plural subjects, "with an S")
-        var feelingPlural = Pick(Feelings); // Frontier: FeelingsPlural < Feelings (not plural subjects, "S")
+        var feelingPlural = Pick(Feelings); // Frontier: FeelingsPlural < Feelings (not plural subjects, "with an S")
         var must = Pick(Musts);
         var require = Pick(Requires);
         var action = Pick(Actions);
