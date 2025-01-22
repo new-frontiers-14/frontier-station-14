c99e50f101d294023e6152d20b4f1580a6268c2f
Dvir <dvirf01@gmail.com>
2025-01-23 00:15:04 +0200

    Cleanup

--- Content.Shared/Storage/Components/MagnetPickupComponent.cs
+++ Content.Shared/Storage/Components/MagnetPickupComponent.cs
@@ -1,34 +1,23 @@
 using Content.Shared.Inventory;
-using Robust.Shared.GameStates;
 
-// namespace Content.Server.Storage.Components;
-namespace Content.Shared.Storage.Components;    // Frontier
+namespace Content.Server.Storage.Components;
 
 /// <summary>
 /// Applies an ongoing pickup area around the attached entity.
 /// </summary>
 [RegisterComponent, AutoGenerateComponentPause]
-[NetworkedComponent, AutoGenerateComponentState] // Frontier
 public sealed partial class MagnetPickupComponent : Component
 {
     [ViewVariables(VVAccess.ReadWrite), DataField("nextScan")]
     [AutoPausedField]
     public TimeSpan NextScan = TimeSpan.Zero;
 
-    [ViewVariables(VVAccess.ReadWrite), DataField("range")]
-    public float Range = 1f;
-
     /// <summary>
-    /// What container slot the magnet needs to be in to work (if not a fixture)
+    /// What container slot the magnet needs to be in to work.
     /// </summary>
     [ViewVariables(VVAccess.ReadWrite), DataField("slotFlags")]
     public SlotFlags SlotFlags = SlotFlags.BELT;
 
-    // Everything below this line is from Frontier
-
-    /// <summary>
-    /// Is the magnet currently enabled?
-    /// </summary>
-    [AutoNetworkedField, ViewVariables(VVAccess.ReadWrite), DataField("magnetEnabled")]
-    public bool MagnetEnabled = true;
+    [ViewVariables(VVAccess.ReadWrite), DataField("range")]
+    public float Range = 1f;
 }
