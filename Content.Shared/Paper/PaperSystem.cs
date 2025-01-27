31d47df0991c53b74a039df81c2abffa794e19e4
Whatstone <whatston3@gmail.com>
2025-01-27 14:46:41 -0500

    More PaperSystem consistency vs. upstream

--- Content.Shared/Paper/PaperSystem.cs
+++ Content.Shared/Paper/PaperSystem.cs
@@ -137,15 +137,18 @@ public sealed class PaperSystem : EntitySystem
         // only allow editing if there are no stamps or when using a cyberpen
         var editable = entity.Comp.StampedBy.Count == 0 || _tagSystem.HasTag(args.Used, "WriteIgnoreStamps")
                        || _tagSystem.HasTag(args.Used, "NFWriteIgnoreUnprotectedStamps") && !_tagSystem.HasTag(entity, "NFPaperStampProtected"); // Frontier: protected stamps
-        if (_tagSystem.HasTag(args.Used, "Write") && editable)
+        if (_tagSystem.HasTag(args.Used, "Write"))
         {
-            // Frontier - Restrict writing to entities with ActorComponent, players only
-            if (!TryComp<ActorComponent>(args.User, out var actor))
-                return;
-            // End Frontier
-
             if (editable)
             {
+                // Frontier - Restrict writing to entities with ActorComponent, players only
+                if (!TryComp<ActorComponent>(args.User, out var actor))
+                {
+                    args.Handled = true;
+                    return;
+                }
+                // End Frontier
+
                 if (entity.Comp.EditingDisabled)
                 {
                     var paperEditingDisabledMessage = Loc.GetString("paper-tamper-proof-modified-message");
@@ -160,9 +163,9 @@ public sealed class PaperSystem : EntitySystem
                 entity.Comp.Mode = PaperAction.Write;
                 _uiSystem.OpenUi(entity.Owner, PaperUiKey.Key, args.User);
                 UpdateUserInterface(entity);
+                args.Handled = true;
+                return;
             }
-            args.Handled = true;
-            return;
         }
 
         // If a stamp, attempt to stamp paper
