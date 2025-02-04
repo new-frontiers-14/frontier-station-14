f80eff702e969bf7fcd8a43b1974fb53d9c48a1d
Whatstone <whatston3@gmail.com>
2025-02-04 18:39:17 -0500

    Revert "cyborg module can't hold blood-red magboots"
    This reverts commit 16e650fdcdc59c4f43d876db4758bd035f787ba4.

--- Content.Server/_NF/Whitelist/Components/NFBorgJetpackComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Server._NF.Whitelist.Components;
-
-/// <summary>
-/// Whitelist component for borg-suitable jetpacks to avoid tag redefinition and collisions
-/// </summary>
-[RegisterComponent]
-public sealed partial class NFBorgJetpackComponent : Component;
