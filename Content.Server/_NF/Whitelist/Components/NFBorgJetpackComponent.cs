16e650fdcdc59c4f43d876db4758bd035f787ba4
Whatstone <whatston3@gmail.com>
2025-02-04 12:08:19 -0500

    cyborg module can't hold blood-red magboots

---
+++ Content.Server/_NF/Whitelist/Components/NFBorgJetpackComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Server._NF.Whitelist.Components;
+
+/// <summary>
+/// Whitelist component for borg-suitable jetpacks to avoid tag redefinition and collisions
+/// </summary>
+[RegisterComponent]
+public sealed partial class NFBorgJetpackComponent : Component;
