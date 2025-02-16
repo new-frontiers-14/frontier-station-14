42944107bf073cf409927a6527251ae5cee716b7
Whatstone <whatston3@gmail.com>
2025-02-16 11:09:49 -0500

    integration test cvar: control.static_storage_ui

--- Content.Shared/CCVar/CCVars.Interactions.cs
+++ Content.Shared/CCVar/CCVars.Interactions.cs
@@ -44,7 +44,7 @@ public sealed partial class CCVars
     ///     Whether or not the storage UI is static and bound to the hotbar, or unbound and allowed to be dragged anywhere.
     /// </summary>
     public static readonly CVarDef<bool> StaticStorageUI =
-        CVarDef.Create("control.static_storage_ui", true, CVar.CLIENTONLY | CVar.ARCHIVE); // Frontier: FIXME, setting this to false actully breaks StorageInteractionTest, since its client settings we can let it be.
+        CVarDef.Create("control.static_storage_ui", false, CVar.CLIENTONLY | CVar.ARCHIVE); // Frontier: true<false
 
     /// <summary>
     ///     Whether or not the storage window uses a transparent or opaque sprite.
