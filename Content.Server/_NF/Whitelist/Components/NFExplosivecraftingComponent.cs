fe0039b5d0949bb9ab2e9e08108a4bc1d01f6f94
Whatstone <whatston3@gmail.com>
2025-02-25 09:34:07 -0500

    NFExplosivecrafting to NFFireBomb

--- Content.Server/_NF/Whitelist/Components/NFExplosivecraftingComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Server._NF.Whitelist.Components;
-
-/// <summary>
-/// Whitelist component for crafting explosive arrow bolts
-/// </summary>
-[RegisterComponent]
-public sealed partial class NFExplosivecraftingComponent : Component;
