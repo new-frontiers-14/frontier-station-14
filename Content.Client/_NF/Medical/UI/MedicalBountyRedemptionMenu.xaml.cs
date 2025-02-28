019e909e43dd01e84cc7c2ba92193f47bd8ca40c
Whatstone <whatston3@gmail.com>
2025-02-28 14:59:23 -0500

    swap the print conditions

--- Content.Client/_NF/Medical/UI/MedicalBountyRedemptionMenu.xaml.cs
+++ Content.Client/_NF/Medical/UI/MedicalBountyRedemptionMenu.xaml.cs
@@ -51,9 +51,9 @@ public sealed partial class MedicalBountyRedemptionMenu : FancyWindow
             case MedicalBountyRedemptionStatus.Valid:
                 BodyLabel.Text = Loc.GetString("medical-bounty-redemption-body-label-valid-bounty");
                 if (message.PaidToStation)
-                    RedeemLabel.Text = Loc.GetString("medical-bounty-redemption-redeem-label-valid-bounty", ("amount", BankSystemExtensions.ToSpesoString(message.BountyValue)));
-                else
                     RedeemLabel.Text = Loc.GetString("medical-bounty-redemption-redeem-label-valid-bounty-to-station", ("amount", BankSystemExtensions.ToSpesoString(message.BountyValue)));
+                else
+                    RedeemLabel.Text = Loc.GetString("medical-bounty-redemption-redeem-label-valid-bounty", ("amount", BankSystemExtensions.ToSpesoString(message.BountyValue)));
                 SellButton.Disabled = false;
                 break;
         }
