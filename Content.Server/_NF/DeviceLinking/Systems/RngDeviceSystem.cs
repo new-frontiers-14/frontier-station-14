53888ac723844ac004175b914d9b428172b0b8fa
shab00m <tobias.sjobom@gmail.com>
2025-03-01 19:37:43 +0100

    Update state/icon when triggered to reflect the number rolled

--- Content.Server/_NF/DeviceLinking/Systems/RngDeviceSystem.cs
+++ Content.Server/_NF/DeviceLinking/Systems/RngDeviceSystem.cs
@@ -3,6 +3,9 @@ using SignalReceivedEvent = Content.Server.DeviceLinking.Events.SignalReceivedEv
 using Robust.Shared.Random;
 using System.Linq;
 using Content.Shared.DeviceLinking;
+using Robust.Server.GameObjects;
+using Robust.Shared.GameObjects;
+using static Content.Shared.DeviceLinking.RngDeviceVisuals;
 
 namespace Content.Server.DeviceLinking.Systems;
 
@@ -10,6 +13,7 @@ public sealed class RngDeviceSystem : EntitySystem
 {
     [Dependency] private readonly DeviceLinkSystem _deviceLink = default!;
     [Dependency] private readonly IRobustRandom _random = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
 
     public override void Initialize()
     {
@@ -48,6 +52,21 @@ public sealed class RngDeviceSystem : EntitySystem
             6 => comp.Output6Port,
             _ => throw new ArgumentOutOfRangeException($"Invalid roll value: {roll}")
         };
+        
+        // Update the appearance to show the current roll
+        if (TryComp<AppearanceComponent>(uid, out var appearance))
+        {
+            var statePrefix = comp.Outputs switch
+            {
+                2 => "percentile",  // Percentile die
+                4 => "d4",         // 4-sided die
+                6 => "d6",         // 6-sided die
+                _ => throw new ArgumentException($"Unsupported number of outputs: {comp.Outputs}")
+            };
+            
+            var stateNumber = comp.Outputs == 2 ? roll * 10 : roll;  // For percentile die, use 10/20 instead of 1/2
+            _appearance.SetData(uid, RngDeviceVisuals.State, $"{statePrefix}_{stateNumber}", appearance);
+        }
         
         _deviceLink.InvokePort(uid, outputPort);
     }
