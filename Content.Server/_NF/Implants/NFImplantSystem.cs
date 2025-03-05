ee4bbb31f5f82636cd79ab754b595565e5c8e28b
Whatstone <whatston3@gmail.com>
2025-03-05 13:14:00 -0500

    Enforce mime illiteracy

--- Content.Server/_NF/Implants/NFImplantSystem.cs
+++ Content.Server/_NF/Implants/NFImplantSystem.cs
@@ -1,10 +1,10 @@
 using Content.Server.Abilities.Mime;
 using Content.Server.Bible.Components;
 using Content.Server.Implants;
-using Content.Shared._NF.Implants;
 using Content.Shared._NF.Implants.Components;
 using Content.Shared.Implants;
 using Content.Shared.Implants.Components;
+using Content.Shared.Paper;
 using Robust.Shared.Containers;
 
 namespace Content.Server._NF.Implants;
@@ -17,10 +17,10 @@ public sealed class NFImplantSystem : EntitySystem
 
         SubscribeLocalEvent<BibleUserImplantComponent, ImplantImplantedEvent>(OnBibleInserted);
         // Need access to the implant, this has to run before the implant is removed.
-        SubscribeLocalEvent<BibleUserImplantComponent, EntGotRemovedFromContainerMessage>(OnBibleRemoved, before: new[] { typeof(SubdermalImplantSystem) });
+        SubscribeLocalEvent<BibleUserImplantComponent, EntGotRemovedFromContainerMessage>(OnBibleRemoved, before: [typeof(SubdermalImplantSystem)]);
         SubscribeLocalEvent<MimePowersImplantComponent, ImplantImplantedEvent>(OnMimeInserted);
         // Need access to the implant, this has to run before the implant is removed.
-        SubscribeLocalEvent<MimePowersImplantComponent, EntGotRemovedFromContainerMessage>(OnMimeRemoved, before: new[] { typeof(SubdermalImplantSystem) });
+        SubscribeLocalEvent<MimePowersImplantComponent, EntGotRemovedFromContainerMessage>(OnMimeRemoved, before: [typeof(SubdermalImplantSystem)]);
     }
 
     private void OnBibleInserted(EntityUid uid, BibleUserImplantComponent component, ImplantImplantedEvent args)
@@ -45,8 +45,12 @@ public sealed class NFImplantSystem : EntitySystem
         if (!args.Implanted.HasValue)
             return;
 
-        var mimeComp = EnsureComp<MimePowersComponent>(args.Implanted.Value);
+        EnsureComp<MimePowersComponent>(args.Implanted.Value, out var mimeComp);
         mimeComp.PreventWriting = true;
+        // Note: must spawn the illiteracy component separately
+        EnsureComp<BlockWritingComponent>(args.Implanted.Value, out var blockWritingComp);
+        blockWritingComp.FailWriteMessage = mimeComp.FailWriteMessage;
+        Dirty(args.Implanted.Value, blockWritingComp);
     }
 
     private void OnMimeRemoved(EntityUid uid, MimePowersImplantComponent component, EntGotRemovedFromContainerMessage args)
