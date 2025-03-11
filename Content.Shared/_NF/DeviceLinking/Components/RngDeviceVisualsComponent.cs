69b89974b6d3417445ca8ab1596586cbe5c61078
shab00m <tobias.sjobom@gmail.com>
2025-03-11 16:13:39 +0100

    Removed empty body for RngDeviceVisualsComponent

--- Content.Shared/_NF/DeviceLinking/Components/RngDeviceVisualsComponent.cs
+++ Content.Shared/_NF/DeviceLinking/Components/RngDeviceVisualsComponent.cs
@@ -3,6 +3,4 @@ using Robust.Shared.GameObjects;
 namespace Content.Shared._NF.DeviceLinking.Components;
 
 [RegisterComponent]
-public sealed partial class RngDeviceVisualsComponent : Component
-{
-}
+public sealed partial class RngDeviceVisualsComponent : Component;
