47ab48080aecc3dbaf6d102f5af2a326af014f26
Whatstone <whatston3@gmail.com>
2025-04-18 09:53:33 -0400

    Restore Nyano RoundNotificationsSystem

---
+++ Content.Server/_NF/RoundNotifications/RoundNotificationsSystem.cs
@@ -0,0 +1,144 @@
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using Content.Server.Discord;
+using Content.Shared._NF.CCVar;
+using Content.Server.Maps;
+using Content.Shared.GameTicking;
+using Robust.Shared;
+using Robust.Shared.Configuration;
+
+namespace Content.Server._NF.RoundNotifications;
+
+/// <summary>
+/// Listen for game events and send notifications to Discord.
+/// </summary>
+/// <remarks>
+/// Updated version of the old Nyanotrasen RoundNotificationsSystem
+/// </remarks>
+public sealed class RoundNotificationsSystem : EntitySystem
+{
+    [Dependency] private readonly IConfigurationManager _config = default!;
+    [Dependency] private readonly IGameMapManager _gameMapManager = default!;
+    [Dependency] private readonly DiscordWebhook _discord = default!;
+
+    private ISawmill _sawmill = default!;
+    private readonly HttpClient _httpClient = new();
+
+    private string _roleId = string.Empty;
+    private bool _roundStartOnly;
+    private string _serverName = string.Empty;
+    private WebhookIdentifier? _webhookIdentifier;
+
+    /// <inheritdoc/>
+    public override void Initialize()
+    {
+        base.Initialize();
+        SubscribeLocalEvent<RoundRestartCleanupEvent>(OnRoundRestart);
+        SubscribeLocalEvent<RoundStartedEvent>(OnRoundStarted);
+        SubscribeLocalEvent<RoundEndMessageEvent>(OnRoundEnded);
+
+        _config.OnValueChanged(NFCCVars.DiscordRoundRoleId, value => _roleId = value, true);
+        _config.OnValueChanged(NFCCVars.DiscordRoundStartOnly, value => _roundStartOnly = value, true);
+        _config.OnValueChanged(CVars.GameHostName, value => _serverName = value, true);
+        Subs.CVar(_config, NFCCVars.DiscordRoundWebhook, value =>
+        {
+            if (!string.IsNullOrWhiteSpace(value))
+                _discord.GetWebhook(value, data => _webhookIdentifier = data.ToIdentifier());
+            else
+                _webhookIdentifier = null;
+        }, true);
+
+        _sawmill = Logger.GetSawmill("notifications");
+    }
+
+    private void OnRoundRestart(RoundRestartCleanupEvent e)
+    {
+        if (_webhookIdentifier == null)
+            return;
+
+        var text = Loc.GetString("discord-round-new");
+
+        SendDiscordMessage(text, true, 0x91B2C7);
+    }
+
+    private void OnRoundStarted(RoundStartedEvent e)
+    {
+        if (_webhookIdentifier == null)
+            return;
+
+        var map = _gameMapManager.GetSelectedMap();
+        var mapName = map?.MapName ?? Loc.GetString("discord-round-unknown-map");
+        var text = Loc.GetString("discord-round-start",
+            ("id", e.RoundId),
+            ("map", mapName));
+
+        SendDiscordMessage(text, false);
+    }
+
+    private void OnRoundEnded(RoundEndMessageEvent e)
+    {
+        if (_webhookIdentifier == null || _roundStartOnly)
+            return;
+
+        var text = Loc.GetString("discord-round-end",
+            ("id", e.RoundId),
+            ("hours", Math.Truncate(e.RoundDuration.TotalHours)),
+            ("minutes", e.RoundDuration.Minutes),
+            ("seconds", e.RoundDuration.Seconds));
+
+        SendDiscordMessage(text, false, 0xB22B27);
+    }
+
+    private async void SendDiscordMessage(string text, bool ping = false, int color = 0x41F097)
+    {
+        if (_webhookIdentifier == null)
+            return;
+
+        try
+        {
+            // Limit server name to 1500 characters, in case someone tries to be a little funny
+            var serverName = _serverName[..Math.Min(_serverName.Length, 1500)];
+            var message = "";
+            if (!string.IsNullOrEmpty(_roleId) && ping)
+                message = $"<@&{_roleId}>";
+
+            // Build the embed
+            var payload = new WebhookPayload
+            {
+                Content = message,
+                Embeds = new List<WebhookEmbed>
+                {
+                    new()
+                    {
+                        Title = Loc.GetString("discord-round-title"),
+                        Description = text,
+                        Color = color,
+                        Footer = new WebhookEmbedFooter
+                        {
+                            Text = $"{serverName}"
+                        },
+                    },
+                },
+            };
+            if (!string.IsNullOrEmpty(_roleId) && ping)
+            {
+                var mention = new WebhookMentions();
+                mention.Roles.Add(_roleId);
+            }
+
+            var request = await _discord.CreateMessage(_webhookIdentifier.Value, payload);
+            if (!request.IsSuccessStatusCode)
+            {
+                var content = await request.Content.ReadAsStringAsync();
+                _sawmill.Log(LogLevel.Error,
+                    $"Discord returned bad status code when posting message: {request.StatusCode}\nResponse: {content}");
+                return;
+            }
+        }
+        catch (Exception e)
+        {
+            Log.Error($"Error while sending discord round status message:\n{e}");
+        }
+    }
+}
