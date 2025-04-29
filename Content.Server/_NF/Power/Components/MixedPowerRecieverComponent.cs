4cd5c5d0a6843568e68d61f70b499c9a0e84aca7
Alkheemist <alkheemist@gmail.com>
2025-04-29 16:52:08 +1000

    switch to acting as a shim inside powercellsystem

--- Content.Server/_NF/Power/Components/MixedPowerRecieverComponent.cs
+++ Content.Server/_NF/Power/Components/MixedPowerRecieverComponent.cs
@@ -4,17 +4,11 @@ using Content.Server.Power.Components;
 namespace Content.Server._NF.Power.Components;
 
 /// <summary>
-///     Attempts to link with a nearby <see cref="ApcPowerProviderComponent"/>s
-///     so that it can receive power from a <see cref="IApcNet"/>.
-///     If no power provider is found, the system will pull power from installed cells instead.
+///     Marks an entity as capable of using both APC and battery power.
 /// </summary>
 [RegisterComponent]
 public sealed partial class MixedPowerReceiverComponent : Component
 {
-    /// <summary>
-     /// How many watts (from the battery) does the device need?
-     /// </summary>
-    [DataField]
-    public float Wattage = 5f;
+
 }
 
