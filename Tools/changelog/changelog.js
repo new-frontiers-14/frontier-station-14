bbe75a74213e9ae10d1aa8f788921efc90486363
Dvir <dvirf01@gmail.com>
2025-05-20 22:40:05 +0300

    Update changelog script for URL inclusion
    Modified the changelog.js script to include a URL field pointing to the pull request. Also updated the HeaderRegex to allow commas in author names.

--- Tools/changelog/changelog.js
+++ Tools/changelog/changelog.js
@@ -7,7 +7,7 @@ const axios = require("axios");
 if (process.env.GITHUB_TOKEN) axios.defaults.headers.common["Authorization"] = `Bearer ${process.env.GITHUB_TOKEN}`;
 
 // Regexes
-const HeaderRegex = /^\s*(?::cl:|🆑) *([a-z0-9_\- ]+)?\s+/im; // :cl: or 🆑 [0] followed by optional author name [1]
+const HeaderRegex = /^\s*(?::cl:|🆑) *([a-z0-9_\-, ]+)?\s+/im; // :cl: or 🆑 [0] followed by optional author name [1]
 const EntryRegex = /^ *[*-]? *(add|remove|tweak|fix): *([^\n\r]+)\r?$/img; // * or - followed by change type [0] and change message [1]
 const CommentRegex = /<!--.*?-->/gs; // HTML comments
 
@@ -57,6 +57,7 @@ async function main() {
         changes: entries,
         id: getHighestCLNumber() + 1,
         time: time,
+        url: `https://github.com/${process.env.GITHUB_REPOSITORY}/pull/${process.env.PR_NUMBER}`,
     };
 
     // Write changelogs
