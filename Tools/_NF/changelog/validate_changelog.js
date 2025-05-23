768bbf474430c981a577bf2ecf24521793191e06
Whatstone <166147148+whatston3@users.noreply.github.com>
2025-05-23 10:05:31 -0400

    Changelog hotfix (#3442)
    * fix, check changelog dir in changelog scripts
    
    * Add missing changelog entries, curb older ones
    
    * changelog entry for erhard
    
    * comment out CHANGELOG_DIR test

--- Tools/_NF/changelog/validate_changelog.js
+++ Tools/_NF/changelog/validate_changelog.js
@@ -8,6 +8,22 @@ const CommentRegex = /<!--.*?-->/gs; // HTML comments
 
 // Main function
 async function main() {
+    // Check changelog directory.
+    // TODO: restore this when we set up the environment variable
+    /*
+    if (!process.env.CHANGELOG_DIR) {
+        console.log("CHANGELOG_DIR not defined, exiting.");
+        return process.exit(1);
+    }
+
+    const ChangelogFilePath = `../../../${process.env.CHANGELOG_DIR}`
+
+    if (!fs.existsSync(ChangelogFilePath)) {
+        console.log(`Cannot find changelog at "${ChangelogFilePath}", exiting.`);
+        return process.exit(1);
+    }
+    */
+
     // Read GitHub event payload
     const eventPath = process.env.GITHUB_EVENT_PATH;
     if (!eventPath) {
