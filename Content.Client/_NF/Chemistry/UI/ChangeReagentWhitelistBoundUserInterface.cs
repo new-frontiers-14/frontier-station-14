58b2173b2ee0666513a1897e8429a8cf4aebd25c
Whatstone <whatston3@gmail.com>
2025-05-25 13:43:19 -0400

    ChangeReagentWhitelist: deindent

--- Content.Client/_NF/Chemistry/UI/ChangeReagentWhitelistBoundUserInterface.cs
+++ Content.Client/_NF/Chemistry/UI/ChangeReagentWhitelistBoundUserInterface.cs
@@ -4,38 +4,37 @@ using JetBrains.Annotations;
 using Robust.Client.UserInterface;
 using Robust.Shared.Prototypes;
 
-namespace Content.Client._NF.Chemistry.UI
+namespace Content.Client._NF.Chemistry.UI;
+
+[UsedImplicitly]
+public sealed class ChangeReagentWhitelistBoundUserInterface : BoundUserInterface
 {
-    [UsedImplicitly]
-    public sealed class ChangeReagentWhitelistBoundUserInterface : BoundUserInterface
+    private ChangeReagentWhitelistWindow? _window;
+    public ChangeReagentWhitelistBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
     {
-        private ChangeReagentWhitelistWindow? _window;
-        public ChangeReagentWhitelistBoundUserInterface(EntityUid owner, Enum uiKey) : base(owner, uiKey)
-        {
-        }
+    }
 
-        protected override void Open()
+    protected override void Open()
+    {
+        base.Open();
+        if (_window == null)
         {
-            base.Open();
-            if (_window == null)
-            {
-                _window = this.CreateWindow<ChangeReagentWhitelistWindow>();
-                _window.SetEntity(Owner);
-                _window.OnChangeWhitelistedReagent += ChangeReagentWhitelist;
-                _window.OnResetWhitelistReagent += ResetReagentWhitelist;
-            }
+            _window = this.CreateWindow<ChangeReagentWhitelistWindow>();
+            _window.SetEntity(Owner);
+            _window.OnChangeWhitelistedReagent += ChangeReagentWhitelist;
+            _window.OnResetWhitelistReagent += ResetReagentWhitelist;
         }
+    }
 
-        public void ChangeReagentWhitelist(ProtoId<ReagentPrototype> newReagentProto)
-        {
-            SendMessage(new ReagentWhitelistChangeMessage(newReagentProto));
-            Close();
-        }
+    public void ChangeReagentWhitelist(ProtoId<ReagentPrototype> newReagentProto)
+    {
+        SendMessage(new ReagentWhitelistChangeMessage(newReagentProto));
+        Close();
+    }
 
-        public void ResetReagentWhitelist()
-        {
-            SendMessage(new ReagentWhitelistResetMessage());
-            Close();
-        }
+    public void ResetReagentWhitelist()
+    {
+        SendMessage(new ReagentWhitelistResetMessage());
+        Close();
     }
 }
