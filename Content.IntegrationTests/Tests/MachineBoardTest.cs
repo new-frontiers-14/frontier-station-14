b6ed2c9b40cef2bfc95553180e7ff1b3669738a7
Whatstone <whatston3@gmail.com>
2025-05-26 10:54:52 -0400

    MachinePart test WIP

--- Content.IntegrationTests/Tests/MachineBoardTest.cs
+++ Content.IntegrationTests/Tests/MachineBoardTest.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Content.Server.Construction.Components;
 using Content.Shared.Construction.Components;
+using Content.Shared.Construction.Prototypes;
+using Content.Shared.Stacks;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Prototypes;
 
@@ -135,6 +137,107 @@ public sealed class MachineBoardTest
                     }
                 });
             }
+        });
+
+        await pair.CleanReturnAsync();
+    }
+
+    // Frontier: 
+    /// <summary>
+    /// Invalid stack types for MachineBoard components, should be listed as requirements.
+    /// </summary>
+    private readonly HashSet<string> _invalidStackTypes = new()
+    {
+    };
+
+    /// <summary>
+    /// Invalid tags for MachineBoard components, should be listed as requirements.
+    /// </summary>
+    private readonly HashSet<string> _invalidTags = new()
+    {
+    };
+
+    /// <summary>
+    /// Invalid components for MachineBoard components, should be listed as requirements.
+    /// </summary>
+    private readonly HashSet<string> _invalidComponents = new()
+    {
+        "PowerCell"
+    };
+
+    /// <summary>
+    /// Check machine requirements for stacks that are machine upgrades.
+    /// </summary>
+    [Test]
+    public async Task TestValidateBoardStackRequirements()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
+
+        var entMan = server.ResolveDependency<IEntityManager>();
+        var protoMan = server.ResolveDependency<IPrototypeManager>();
+        var compFact = server.ResolveDependency<IComponentFactory>();
+
+        await server.WaitAssertion(() =>
+        {
+            HashSet<EntProtoId> machinePartEntities = new();
+            foreach (var p in protoMan.EnumeratePrototypes<MachinePartPrototype>()
+                                .Where(p => !pair.IsTestPrototype(p))
+                                .Where(p => !_ignoredPrototypes.Contains(p.ID)))
+            {
+                machinePartEntities.Add(p.StockPartPrototype);
+            }
+
+            Assert.Multiple(() =>
+            {
+                foreach (var p in protoMan.EnumeratePrototypes<EntityPrototype>()
+                            .Where(p => !p.Abstract)
+                            .Where(p => !pair.IsTestPrototype(p))
+                            .Where(p => !_ignoredPrototypes.Contains(p.ID)))
+                {
+                    if (!p.TryGetComponent<MachineBoardComponent>(out var mbc, compFact))
+                        continue;
+
+                    foreach (var stackReq in mbc.StackRequirements.Keys)
+                    {
+                        if (_invalidStackTypes.Contains(stackReq))
+                        {
+                            Assert.Fail($"Entity {p.ID} has a stackRequirement for {stackReq}, which should be converted into a machine part requirement.");
+                            continue;
+                        }
+
+                        if (!protoMan.TryIndex(stackReq, out var stack))
+                        {
+                            Assert.Fail($"Entity {p.ID} has a stackRequirement for {stackReq}, which could not be resolved.");
+                            continue;
+                        }
+
+                        if (machinePartEntities.Contains(stack.Spawn))
+                        {
+                            Assert.Fail($"Entity {p.ID} has a stackRequirement for {stackReq}, which is a machine part, and should be in requirements.");
+                            continue;
+                        }
+                    }
+
+                    foreach (var tagReq in mbc.TagRequirements.Keys)
+                    {
+                        if (_invalidTags.Contains(tagReq))
+                        {
+                            Assert.Fail($"Entity {p.ID} has a tagRequirement for {tagReq}, which should be listed into a machine part requirement.");
+                            continue;
+                        }
+                    }
+
+                    foreach (var compReq in mbc.ComponentRequirements.Keys)
+                    {
+                        if (_invalidComponents.Contains(compReq))
+                        {
+                            Assert.Fail($"Entity {p.ID} has a componentRequirement for {compReq}, which should be converted into a machine part requirement.");
+                            continue;
+                        }
+                    }
+                }
+            });
         });
 
         await pair.CleanReturnAsync();
