e0476af0bd8990ddc34e6ee162d620609fcbf896
dvir001 <dvirf01@gmail.com>
2025-06-01 15:05:46 +0300

    Added Scandinavian Accent
    https://github.com/space-wizards/space-station-14/pull/35935

---
+++ Content.Server/_NF/Speech/Components/ScandinavianAccentComponent.cs
@@ -0,0 +1,7 @@
+using Content.Server._NF.Speech.EntitySystems;
+
+namespace Content.Server._NF.Speech.Components;
+
+[RegisterComponent]
+[Access(typeof(ScandinavianAccentSystem))]
+public sealed partial class ScandinavianAccentComponent : Component;
