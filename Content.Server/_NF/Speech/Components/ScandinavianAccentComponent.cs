59927c99d5ef1cf995e075b2ccbdfbdf3b957a09
Dvir <39403717+dvir001@users.noreply.github.com>
2025-06-05 09:20:02 +0300

    Update Content.Server/_NF/Speech/Components/ScandinavianAccentComponent.cs
    Co-authored-by: Whatstone <166147148+whatston3@users.noreply.github.com>

--- Content.Server/_NF/Speech/Components/ScandinavianAccentComponent.cs
+++ Content.Server/_NF/Speech/Components/ScandinavianAccentComponent.cs
@@ -3,5 +3,4 @@ using Content.Server._NF.Speech.EntitySystems;
 namespace Content.Server._NF.Speech.Components;
 
 [RegisterComponent]
-[Access(typeof(ScandinavianAccentSystem))]
 public sealed partial class ScandinavianAccentComponent : Component;
