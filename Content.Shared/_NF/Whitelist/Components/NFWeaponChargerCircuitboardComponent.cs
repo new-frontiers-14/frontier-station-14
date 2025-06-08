e4665e25bbb8b5cabaf4ef04909e960b7d3e4917
Whatstone <166147148+whatston3@users.noreply.github.com>
2025-06-08 09:43:47 -0400

    Fix weapon recharger board whitelist comment

--- Content.Shared/_NF/Whitelist/Components/NFWeaponChargerCircuitboardComponent.cs
+++ Content.Shared/_NF/Whitelist/Components/NFWeaponChargerCircuitboardComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Shared._NF.Whitelist.Components;
 
 /// <summary>
-/// Whitelist component for cigar cases and cigarette packets to avoid tag redefinition and collisions
+/// Whitelist component for weapon recharger circuit boards to avoid tag redefinition and collisions
 /// </summary>
 [RegisterComponent]
 public sealed partial class NFWeaponChargerCircuitboardComponent : Component;
