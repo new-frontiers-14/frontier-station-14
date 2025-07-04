962437e39d7eef80ec7bdb6d147c5052a050585f
dvir001 <dvirf01@gmail.com>
2025-07-04 19:54:09 +0300

    Refactor research tech tree UI with scroll and improved lines (WIP)
    Right now there is a render issue with the prerequisite linking lines hidden, to be fixed.

--- Content.Client/_NF/Research/UI/ResearchesContainerPanel.cs
+++ Content.Client/_NF/Research/UI/ResearchesContainerPanel.cs
@@ -16,6 +16,16 @@ public sealed partial class ResearchesContainerPanel : LayoutContainer
 
     protected override void Draw(DrawingHandleScreen handle)
     {
+        // First draw all children (including parallax background and tech items)
+        base.Draw(handle);
+
+        // Then draw prerequisite lines ON TOP of everything for maximum visibility
+        DrawPrerequisiteLines(handle);
+    }
+
+    private void DrawPrerequisiteLines(DrawingHandleScreen handle)
+    {
+        // Draw prerequisite lines with maximum visibility - bright colors and thick lines
         foreach (var child in Children)
         {
             if (child is not FancyResearchConsoleItem item)
@@ -24,22 +34,37 @@ public sealed partial class ResearchesContainerPanel : LayoutContainer
             if (item.Prototype.TechnologyPrerequisites.Count <= 0)
                 continue;
 
-            var list = Children.Where(x => x is FancyResearchConsoleItem second && item.Prototype.TechnologyPrerequisites.Contains(second.Prototype.ID));
-            foreach (var second in list)
+            var prerequisiteItems = Children.Where(x => x is FancyResearchConsoleItem second &&
+                item.Prototype.TechnologyPrerequisites.Contains(second.Prototype.ID));
+
+            foreach (var prerequisiteItem in prerequisiteItems)
             {
-
                 var startCoords = new Vector2(item.PixelPosition.X + item.PixelWidth / 2, item.PixelPosition.Y + item.PixelHeight / 2);
-                var endCoords = new Vector2(second.PixelPosition.X + second.PixelWidth / 2, second.PixelPosition.Y + second.PixelHeight / 2);
+                var endCoords = new Vector2(prerequisiteItem.PixelPosition.X + prerequisiteItem.PixelWidth / 2, prerequisiteItem.PixelPosition.Y + prerequisiteItem.PixelHeight / 2);
 
-                if (second.PixelPosition.Y != item.PixelPosition.Y)
-                {
+                // Use bright magenta for absolute maximum visibility against any background
+                var lineColor = Color.Magenta;
 
-                    handle.DrawLine(startCoords, new(endCoords.X, startCoords.Y), Color.White);
-                    handle.DrawLine(new(endCoords.X, startCoords.Y), endCoords, Color.White);
-                }
-                else
+                // Draw very thick lines by using a larger offset grid
+                for (int x = -3; x <= 3; x++)
                 {
-                    handle.DrawLine(startCoords, endCoords, Color.White);
+                    for (int y = -3; y <= 3; y++)
+                    {
+                        var offsetStartCoords = startCoords + new Vector2(x, y);
+                        var offsetEndCoords = endCoords + new Vector2(x, y);
+
+                        if (prerequisiteItem.PixelPosition.Y != item.PixelPosition.Y)
+                        {
+                            // Draw L-shaped connection for different Y positions
+                            handle.DrawLine(offsetStartCoords, new(offsetEndCoords.X, offsetStartCoords.Y), lineColor);
+                            handle.DrawLine(new(offsetEndCoords.X, offsetStartCoords.Y), offsetEndCoords, lineColor);
+                        }
+                        else
+                        {
+                            // Draw direct line for same Y position
+                            handle.DrawLine(offsetStartCoords, offsetEndCoords, lineColor);
+                        }
+                    }
                 }
             }
         }
