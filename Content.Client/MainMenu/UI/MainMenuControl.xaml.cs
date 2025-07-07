6dd34f1ab01e4543ee757746654ff5c99f619183
Josiah Goodman <siahborg@gmail.com>
2025-07-07 01:48:46 -0400

    Added custom splash screen and icon, again courtesy of Cegmeister

--- Content.Client/MainMenu/UI/MainMenuControl.xaml.cs
+++ Content.Client/MainMenu/UI/MainMenuControl.xaml.cs
@@ -22,7 +22,7 @@ public sealed partial class MainMenuControl : Control
         LayoutContainer.SetMarginTop(VBox, 30);
         LayoutContainer.SetGrowHorizontal(VBox, LayoutContainer.GrowDirection.Begin);
 
-        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_NF/Logo/logo.png"); // Frontier: add _NF
+        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_Scav/Logo/logo.png"); // Scav: add _Scav
         Logo.Texture = logoTexture;
 
         var currentUserName = configMan.GetCVar(CVars.PlayerName);
