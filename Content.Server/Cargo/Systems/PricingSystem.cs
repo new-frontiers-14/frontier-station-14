40d76dc93b81b9600d8c6027e289ec4573a83897
Checkraze <71046427+Cheackraze@users.noreply.github.com>
2025-07-06 20:15:43 -0400

    pricing system

--- Content.Server/Cargo/Systems/PricingSystem.cs
+++ Content.Server/Cargo/Systems/PricingSystem.cs
@@ -31,6 +31,7 @@ public sealed class PricingSystem : EntitySystem
 {
     [Dependency] private readonly IConsoleHost _consoleHost = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly IComponentFactory _factory = default!;
     [Dependency] private readonly BodySystem _bodySystem = default!;
     [Dependency] private readonly MobStateSystem _mobStateSystem = default!;
     [Dependency] private readonly SharedSolutionContainerSystem _solutionContainerSystem = default!;
