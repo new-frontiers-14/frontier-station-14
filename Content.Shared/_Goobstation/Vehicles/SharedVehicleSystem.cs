280754b05c7034adfd406ac9ca308a582fdf8aeb
Alkheemist <alkheemist@gmail.com>
2025-07-27 13:06:12 +1000

    comment and unused include fixes

--- Content.Shared/_Goobstation/Vehicles/SharedVehicleSystem.cs
+++ Content.Shared/_Goobstation/Vehicles/SharedVehicleSystem.cs
@@ -13,9 +13,7 @@ using Robust.Shared.Audio.Systems;
 using Robust.Shared.Containers;
 using Content.Shared._NF.Vehicle.Components; // Frontier
 using Content.Shared.ActionBlocker; // Frontier
-using Content.Shared.ActionBlocker;
 using Content.Shared.Actions.Components; // Frontier
-using Content.Shared.Interaction; // Frontier
 using Content.Shared.Light.Components; // Frontier
 using Content.Shared.Light.EntitySystems; // Frontier
 using Content.Shared.Movement.Pulling.Components; // Frontier
