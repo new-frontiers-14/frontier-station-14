1ea741ac287843619404bf545995c9116df7f2a1
bitcrushing <63355298+bitcrushing@users.noreply.github.com>
2025-08-21 18:28:34 +0100

    Gas Multifilter Port (#1971)
    * Porting multigas filter PR from Funky Station
    
    * Annotated changes
    
    * chore: automatically update REUSE headers
    
    ---------
    
    Co-authored-by: Steve <marlumpy@gmail.com>
    Co-authored-by: monolith8319 <195513600+monolith8319@users.noreply.github.com>

--- Content.Client/Atmos/UI/GasFilterWindow.xaml.cs
+++ Content.Client/Atmos/UI/GasFilterWindow.xaml.cs
@@ -1,3 +1,17 @@
+// SPDX-FileCopyrightText: 2021 ike709
+// SPDX-FileCopyrightText: 2022 Paul Ritter
+// SPDX-FileCopyrightText: 2022 Pieter-Jan Briers
+// SPDX-FileCopyrightText: 2022 Vordenburg
+// SPDX-FileCopyrightText: 2022 mirrorcult
+// SPDX-FileCopyrightText: 2022 wrexbe
+// SPDX-FileCopyrightText: 2023 Visne
+// SPDX-FileCopyrightText: 2024 Kot
+// SPDX-FileCopyrightText: 2024 Nemanja
+// SPDX-FileCopyrightText: 2024 metalgearsloth
+// SPDX-FileCopyrightText: 2025 Steve
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using System;
 using System.Collections.Generic;
 using System.Globalization;
