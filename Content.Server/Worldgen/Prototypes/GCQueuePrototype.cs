df37ec079379649b6c4b7dffa75cb2aae5c5221b
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-09-05 00:32:20 -0400

    commenting

--- Content.Server/Worldgen/Prototypes/GCQueuePrototype.cs
+++ Content.Server/Worldgen/Prototypes/GCQueuePrototype.cs
@@ -20,10 +20,10 @@ public sealed class GCQueuePrototype : IPrototype
     public int Depth { get; }
 
     /// <summary>
-    ///     How many miliseconds to spend deleting objects per object in the queue above the MinDepth? Mono Dynamic Queueing
+    ///     How many miliseconds to spend deleting objects per object in the queue above the MinDepth? Mono Dynamic Queueing. This does not override the overall GCQueueSystem timer.
     /// </summary>
     [DataField]
-    public double TimeDeletePerObject { get; } = 0.1; // Mono - at 100 objects past the MinDepth will spend up to 10 milliseconds trying to do deletions
+    public double TimeDeletePerObject { get; } = 0.1; // Mono - at 100 objects past the MinDepth will spend up to 10 milliseconds trying to do deletion. 
 
     /// <summary>
     ///     The minimum depth before entities in the queue actually get processed for deletion.
