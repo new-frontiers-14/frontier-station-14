442dd43d0c98ee0264825b22c0fe727be11ff046
slarticodefast <161409025+slarticodefast@users.noreply.github.com>
2025-09-20 21:05:57 +0200

    Fix RCD errors (#40278)

--- Content.Client/RCD/RCDMenuBoundUserInterface.cs
+++ Content.Client/RCD/RCDMenuBoundUserInterface.cs
@@ -130,7 +130,8 @@ public sealed class RCDMenuBoundUserInterface : BoundUserInterface
             var name = Loc.GetString(proto.SetName);
 
             if (proto.Prototype != null &&
-                _prototypeManager.TryIndex(proto.Prototype, out var entProto, logError: false))
+                _prototypeManager.TryIndex(proto.Prototype, out var entProto)) // don't use Resolve because this can be a tile
+            {
                 name = entProto.Name;
 
             msg = Loc.GetString("rcd-component-change-build-mode", ("name", name));
@@ -147,7 +148,7 @@ public sealed class RCDMenuBoundUserInterface : BoundUserInterface
 
         if (proto.Mode is RcdMode.ConstructTile or RcdMode.ConstructObject
             && proto.Prototype != null
-            && _prototypeManager.TryIndex(proto.Prototype, out var entProto, logError: false))
+            && _prototypeManager.Resolve(proto.Prototype, out var entProto))
         {
             tooltip = Loc.GetString(entProto.Name);
         }
