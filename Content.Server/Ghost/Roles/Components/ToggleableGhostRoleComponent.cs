ee92273e087d5e1e9b96de1fbf479bc8c1ff101c
Narvath <60667511+Thrag-Zan@users.noreply.github.com>
2026-03-05 14:45:59 +0100

    Add Whitelist Support to ToggleableGhostRole and the mentorpAI

--- Content.Server/Ghost/Roles/Components/ToggleableGhostRoleComponent.cs
+++ Content.Server/Ghost/Roles/Components/ToggleableGhostRoleComponent.cs
@@ -1,5 +1,6 @@
 using Content.Shared.Roles;
 using Robust.Shared.Prototypes;
+using Content.Shared.Ghost.Roles;
 
 namespace Content.Server.Ghost.Roles.Components;
 
@@ -86,4 +87,12 @@ public sealed partial class ToggleableGhostRoleComponent : Component
     /// </summary>
     [DataField("job")]
     public ProtoId<JobPrototype>? JobProto;
+
+    // Frontier
+    /// <summary>
+    ///     Whether or not the ghost role requires a player to be whitelisted.
+    /// </summary>
+    [DataField]
+    public bool Whitelisted = false;
+    // End Frontier
 }
