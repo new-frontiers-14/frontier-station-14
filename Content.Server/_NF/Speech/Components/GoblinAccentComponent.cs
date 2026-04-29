bfe7de728633c5a1b2562e6f8286a5e2ca25f48a
steinhauer.erhard <steinhauer.erhard@gmail.com>
2026-04-30 00:39:22 +0300

    Added commentaries to some whitelist components

--- Content.Server/_NF/Speech/Components/GoblinAccentComponent.cs
+++ Content.Server/_NF/Speech/Components/GoblinAccentComponent.cs
@@ -2,5 +2,8 @@ using Content.Server._NF.Speech.EntitySystems;
 
 namespace Content.Server._NF.Speech.Components;
 
+/// <summary>
+/// Applies goblin cant accent to user.
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinAccentComponent : Component;
--- Content.Shared/_NF/Species/Components/GoblinComponent.cs
+++ Content.Shared/_NF/Species/Components/GoblinComponent.cs
@@ -1,19 +1,37 @@
 namespace Content.Server._NF.Species.Components;
 
+/// <summary>
+/// Whitelist component for goblin species to avoid tag redefinition and collisions
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinComponent : Component { }
 
+/// <summary>
+/// Whitelist component for items crafted by goblins to avoid tag redefinition and collisions
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinMadeComponent : Component { }
 
+/// <summary>
+/// Whitelist component for construction graph for goblin robes to avoid tag redefinition and collisions
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinClothingRobesComponent : Component { }
 
+/// <summary>
+/// Whitelist component for construction graphs using dumpster to avoid tag redefinition and collisions
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinVehicleComponent : Component { }
 
+/// <summary>
+/// Whitelist component for items used in goblin crafts to avoid tag redefinition and collisions
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinPreciousTrashComponent : Component { }
 
+/// <summary>
+/// Whitelist component for construction graphs using trash bags to avoid tag redefinition and collisions
+/// </summary>
 [RegisterComponent]
 public sealed partial class GoblinPreciousTrashBagComponent : Component { }
