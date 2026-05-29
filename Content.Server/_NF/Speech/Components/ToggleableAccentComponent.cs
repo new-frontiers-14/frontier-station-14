cf54f082e781e6b2075ad086b47a6268d37cfd3c
RogueVoidling <76178426+RogueVoidling@users.noreply.github.com>
2026-05-29 00:46:25 -0500

    Refactor names to a generic togglable accent system

--- Content.Server/_NF/Speech/Components/ToggleableAccentComponent.cs
+++ Content.Server/_NF/Speech/Components/ToggleableAccentComponent.cs
@@ -1,18 +1,25 @@
 using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
 
 namespace Content.Server._NF.Speech.Components;
 
 [RegisterComponent]
-public sealed partial class PreSapienceAccentComponent : Component
+public sealed partial class ToggleableAccentComponent : Component
 {
-    [DataField]
-    public Component PreSapienceAccentComp;
-
     //Plagiarized from VocalComponent
     [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<EntityPrototype>))]
     public string? ToggleAccentActionId = "ActionTogglePreSapienceAccent";
-
+    //This too
     [DataField]
     public EntityUid? ToggleAccentAction;
+
+    [DataField]
+    public Type AccentComponentType;
+
+    [DataField]
+    public bool IsAccentActive;
+
+    [DataField(customTypeSerializer: typeof(ComponentNameSerializer))]
+    public string AccentComponentName;
 }
--- Content.Server/_NF/Speech/EntitySystems/ToggleableAccentSystem.cs
+++ Content.Server/_NF/Speech/EntitySystems/ToggleableAccentSystem.cs
@@ -2,7 +2,7 @@ using Linguini.Syntax.Ast;
 
 namespace Content.Server._NF.Speech.EntitySystems;
 
-public sealed class PreSapienceAccentSystem : EntitySystem
+public sealed class ToggleableAccentSystem : EntitySystem
 {
 
     public override void Initialize()
