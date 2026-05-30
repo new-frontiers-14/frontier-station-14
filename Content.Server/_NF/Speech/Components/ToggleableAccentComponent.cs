718d7627fb18cd6afa67e98af340e0b836944f73
RogueVoidling <76178426+RogueVoidling@users.noreply.github.com>
2026-05-29 21:41:31 -0500

    Plagirized code from the AddAccentPickupSystem as there's no point to reinvent the wheel

--- Content.Server/_NF/Speech/Components/ToggleableAccentComponent.cs
+++ Content.Server/_NF/Speech/Components/ToggleableAccentComponent.cs
@@ -1,3 +1,4 @@
+using Content.Server.Speech.Prototypes;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
 using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
@@ -15,23 +16,24 @@ public sealed partial class ToggleableAccentComponent : Component
     public EntityUid? ToggleAccentAction;
 
     [DataField]
-    public Type AccentComponentType;
-
-    [DataField]
     public bool IsAccentActive;
 
     [DataField(customTypeSerializer: typeof(ComponentNameSerializer))]
     public string AccentComponentName;
 
+    [DataField(customTypeSerializer: typeof(PrototypeIdSerializer<ReplacementAccentPrototype>))]
+    public string? ReplacementAccentPrototypeName = null;
 
-    //TODO: Evaluate if this is needed
-    //Probably would be nice to have tbh
+
     /// <summary>
-    /// What should happen to the accent if this component were removed
+    /// What should happen to the accent if this component were removed.
+    /// <remarks>DEFAULT should never be manually set by the system,
+    /// it should only be used if a RemovalBehavior was not set by something else.</remarks>
     /// </summary>
     public enum OnRemovalBehavior
     {
         ADD, //If the component is removed, the accent will be added
-        REMOVE //If the component is removed, the accent will be removed
+        REMOVE, //If the component is removed, the accent will be removed
+        DEFAULT //No action will be taken if the component is removed. This should never be set manually
     }
 }
--- Content.Server/_NF/Speech/EntitySystems/ToggleableAccentSystem.cs
+++ Content.Server/_NF/Speech/EntitySystems/ToggleableAccentSystem.cs
@@ -1,5 +1,6 @@
 using Content.Server._NF.Speech.Components;
 using Content.Server._NF.Speech.Events;
+using Content.Server.Speech.Components;
 using Linguini.Syntax.Ast;
 
 namespace Content.Server._NF.Speech.EntitySystems;
@@ -12,7 +13,8 @@ namespace Content.Server._NF.Speech.EntitySystems;
 public sealed class ToggleableAccentSystem : EntitySystem
 {
 
-    [Dependency] private IEntityManager _entMan = default!;
+    [Dependency] private IEntityManager _entityManager = default!;
+    [Dependency] private IComponentFactory _componentFactory = default!;
 
     //TODO: Is it possible for there to ever be two of this applied at once?
     //No, consider writing it to support multipule if we can figure out actions
@@ -20,7 +22,6 @@ public sealed class ToggleableAccentSystem : EntitySystem
     {
         base.Initialize();
         SubscribeLocalEvent<ToggleableAccentComponent, ToggleAccentActionEvent>(OnToggleAction);
-
     }
 
 
@@ -38,15 +39,33 @@ public sealed class ToggleableAccentSystem : EntitySystem
         }
     }
 
-    //TODO: Look at plagiarizing code from AddAccentPickupSystem
+    //Code stolen from AddAccentPickupSystem and lightly edited
     private void ApplyAccent(EntityUid target, ToggleableAccentComponent comp)
     {
+        // does the user already has this accent?
+        var componentType = _componentFactory.GetRegistration(comp.AccentComponentName).Type;
+        if (HasComp(target, componentType))
+            return;
 
+        // add accent to the user
+        var accentComponent = (Component)_componentFactory.GetComponent(componentType);
+        AddComp(target, accentComponent);
+
+        // special case for replacement accent
+        if (accentComponent is ReplacementAccentComponent rep)
+            rep.Accent = comp.ReplacementAccentPrototypeName!;
+
+        comp.IsAccentActive = true;
     }
 
+    //Code stolen from AddAccentPickupSystem with light edits
     private void RemoveAccent(EntityUid target, ToggleableAccentComponent comp)
     {
+        // try to remove accent
+        var componentType = _componentFactory.GetRegistration(comp.AccentComponentName).Type;
+        RemComp(target, componentType);
 
+        comp.IsAccentActive = false;
     }
 
 
