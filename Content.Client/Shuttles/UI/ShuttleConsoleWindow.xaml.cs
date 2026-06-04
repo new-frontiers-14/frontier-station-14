e2405ee256784defd61835db3612278455625ba3
blackknight954 <40075385+blackknight954@users.noreply.github.com>
2026-06-04 14:28:26 -0500

    Updated missed comment

--- Content.Client/Shuttles/UI/ShuttleConsoleWindow.xaml.cs
+++ Content.Client/Shuttles/UI/ShuttleConsoleWindow.xaml.cs
@@ -71,12 +71,12 @@ public sealed partial class ShuttleConsoleWindow : FancyWindow,
         {
             UndockRequest?.Invoke(entity);
         };
-
+    // Frontier
         DockContainer.UndockAllRequest += dockEntities =>
         {
             UndockAllRequest?.Invoke(dockEntities);
         };
-
+    // End Frontier
         NfInitialize(); // Frontier Initialization for the ShuttleConsoleWindow
     }
 
