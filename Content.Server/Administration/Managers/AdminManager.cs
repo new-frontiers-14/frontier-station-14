2bab9cf5e8e23f3670824a00aedc493a8255f917
Kvantos0 <196930377+Kvantos0@users.noreply.github.com>
2026-06-04 16:08:25 +0200

    add proper comments oop

--- Content.Server/Administration/Managers/AdminManager.cs
+++ Content.Server/Administration/Managers/AdminManager.cs
@@ -389,13 +389,14 @@ namespace Content.Server.Administration.Managers
             };
 
             _admins.Add(session, reg);
+            // Frontier begin
             var contentdata = session.ContentData();
             if (contentdata == null)
             {
                 return;
             }
-
-            if (contentdata.Stealthed)
+            // Frontier end
+            if (contentdata.Stealthed) // Frontier session.ContentData()!<contentdata
                 reg.Data.Stealth = true;
 
             if (reg.Data.Active)
