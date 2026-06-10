e19ee26e4fabf804cc3008c2b4efde1ed40c6c38
Herbtheon <alynxyokai@gmail.com>
2026-06-09 23:19:06 -0500

    Adds a Centcom Shipyard alongside with a tabletop version, adding one to the minicentcom grid changed to map. Added the fishbowl and warden to the shipyard console. Do /mapping, then load the MiniCentcom map to test any changes to validate the map.
    Centcom ships are subject to allow FTL travel
    when holding the CCO/ERT ID. Only allowing
    FTL travel to the Centcom outpost.

--- Content.Shared/_NF/Shipyard/SharedShipyardSystem.cs
+++ Content.Shared/_NF/Shipyard/SharedShipyardSystem.cs
@@ -19,6 +19,7 @@ public enum ShipyardConsoleUiKey : byte
     Scrap,
     Sr,
     Medical,
+    Centcom,
     // Add ships to this key if they are only available from mothership consoles. Shipyards using it are inherently empty and are populated using the ShipyardListingComponent.
     Custom
 }
