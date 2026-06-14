4aec1ea1d7d591a5943c781e99dbf48f06c6490b
blackknight954 <40075385+blackknight954@users.noreply.github.com>
2026-06-14 11:37:30 -0500

    One more End Mono comment.

--- Content.Shared/Construction/Components/MachineBoardComponent.cs
+++ Content.Shared/Construction/Components/MachineBoardComponent.cs
@@ -45,6 +45,7 @@ public sealed partial class MachineBoardComponent : Component
     /// Mono - sets the framesize this board can go into
     [DataField]
     public string? FrameSize = null;
+    /// End Mono
 }
 
 /// <summary>
