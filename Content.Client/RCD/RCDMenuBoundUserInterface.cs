da0a5ab092891f9e7ab8bbcc9cc1e1ba9ef29806
blackknight954 <40075385+blackknight954@users.noreply.github.com>
2026-07-24 21:28:09 -0500

    Fixed indenting in RCDBoundUserInterface.

--- Content.Client/RCD/RCDMenuBoundUserInterface.cs
+++ Content.Client/RCD/RCDMenuBoundUserInterface.cs
@@ -24,11 +24,11 @@ public sealed class RCDMenuBoundUserInterface : BoundUserInterface
             ["Airlocks"] = ("rcd-component-airlocks", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/airlocks.png"))),
             ["Electrical"] = ("rcd-component-electrical", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/multicoil.png"))),
             ["Lighting"] = ("rcd-component-lighting", new SpriteSpecifier.Texture(new ResPath("/Textures/Interface/Radial/RCD/lighting.png"))),
-            /// Frontier - Pirate RCD
+            // Frontier - Pirate RCD
             ["PirateWallsAndFlooring"] = ("rcd-component-pirate-walls-and-flooring", new SpriteSpecifier.Texture(new ResPath("/Textures/_NF/Interface/Radial/RCD/walls_and_flooring.png"))),
             ["PirateWindowsAndGrilles"] = ("rcd-component-pirate-windows-and-grilles", new SpriteSpecifier.Texture(new ResPath("/Textures/_NF/Interface/Radial/RCD/windows_and_grilles.png"))),
             ["PirateAirlocks"] = ("rcd-component-pirate-airlocks", new SpriteSpecifier.Texture(new ResPath("/Textures/_NF/Interface/Radial/RCD/airlocks.png"))),
-            /// End Frontier
+            // End Frontier
         };
 
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
@@ -133,6 +133,7 @@ public sealed class RCDMenuBoundUserInterface : BoundUserInterface
                 _prototypeManager.TryIndex(proto.Prototype, out var entProto)) // don't use Resolve because this can be a tile
             {
                 name = entProto.Name;
+            }
 
             msg = Loc.GetString("rcd-component-change-build-mode", ("name", name));
         }
